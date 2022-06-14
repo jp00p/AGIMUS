310ab64b9d9b2af68b2710be4a3e493a2c72d39e
Zane Mattingly <z@matting.ly>
2022-06-13 21:17:29 -0700

    add `clip`/`clips` commands, move drop/clip emoji into config

--- commands/clip.py
+++ commands/clip.py
@@ -4,106 +4,101 @@ from os.path import exists
 from .common import *
 from utils.check_channel_access import *
 
-command_config = config["commands"]["drop"]
+command_config = config["commands"]["clip"]
 emojis = config["emojis"]
 
 # Load JSON Data
 f = open(command_config["data"])
-drop_data = json.load(f)
+clip_data = json.load(f)
 f.close()
 
-# drop() - Entrypoint for !drop command
-# This now just informs the channel that they can use the slash command instead
-async def drop(message:discord.Message):
-  await message.channel.send("No need for !drop any longer, try using the slash command `/drop`! <:nechayev_point_yes:840252104056766515>")
-
-# slash_drops() - Entrypoint for /drops command
-# List the available drops by key and send to user as ephemeral
+# clip() - Entrypoint for /clip command
+# List the available clips by key and send to user as ephemeral
 @slash.slash(
-  name="drops",
-  description="Retrieve the List of Drops.",
+  name="clips",
+  description="Retrieve the List of Clips.",
   guild_ids=config["guild_ids"]
 )
-async def slash_drops(ctx:SlashContext):
-  drops_list = "\n".join(drop_data)
+async def clips(ctx:SlashContext):
+  clips_list = "\n".join(clip_data)
   embed = discord.Embed(
-    title="List of Drops",
-    description=drops_list,
+    title="List of Clips",
+    description=clips_list,
     color=discord.Color.blue()
   )
   try:
     await ctx.author.send(embed=embed)
-    await ctx.reply(f"{emojis['tendi_smile_happy']} Sent you a DM with the full List of Drops!", hidden=True)
+    await ctx.reply(f"{emojis['tendi_smile_happy']} Sent you a DM with the full List of Clips!", hidden=True)
   except:
     await ctx.reply(embed=embed, hidden=True)
 
 
-# slash_drop() - Entrypoint for /drops command
+# clip() - Entrypoint for /clip command
 # Parses a query, determines if it's allowed in the channel,
 # and if allowed retrieve from metadata to do matching and
 # then send the .mp4 file
 @slash.slash(
-  name="drop",
-  description="Send a drop to the channel!",
+  name="clip",
+  description="Send a clip to the channel!",
   guild_ids=config["guild_ids"],
   options=[
     create_option(
       name="query",
-      description="Which drop?",
+      description="Which clip?",
       required=True,
       option_type=3
     ),
     create_option(
       name="private",
-      description="Send drop to just yourself?",
+      description="Send clip to just yourself?",
       required=False,
       option_type=5,
     )
   ]
 )
 @check_channel_access(command_config)
-async def slash_drop(ctx:SlashContext, **kwargs):
+async def clip(ctx:SlashContext, **kwargs):
   query = kwargs.get('query')
   private = kwargs.get('private')
 
   # Private drops are not on the timer
-  drop_allowed = True
+  clip_allowed = True
   if not private:
-    drop_allowed = await check_timekeeper(ctx)
+    clip_allowed = await check_timekeeper(ctx)
 
-  if (drop_allowed):  
+  if (clip_allowed):  
     q = query.lower().strip()
-    drop_metadata = get_drop_metadata(q)
+    clip_metadata = get_clip_metadata(q)
 
-    if drop_metadata:
+    if clip_metadata:
       try:
-        filename = get_mp4(drop_metadata)
+        filename = get_mp4(clip_metadata)
         await ctx.send(file=discord.File(filename), hidden=private)
         if not private:
           set_timekeeper(ctx)
       except BaseException as err:
-        logger.info(f"ERROR LOADING DROP: {err}")
+        logger.info(f"ERROR LOADING CLIP: {err}")
         userid = command_config.get("error_contact_id")
         if userid:
           await ctx.send(f"{emojis['emh_doctor_omg_wtf_zoom']} Something has gone horribly awry, we may have a coolant leak. Contact Lieutenant Engineer <@{userid}>", hidden=True)  
     else:
-      await ctx.send(f"{emojis['ezri_frown_sad']} Drop not found! To get a list of drops run: /drops", hidden=True)
+      await ctx.send(f"{emojis['ezri_frown_sad']} Clip not found! To get a list of clips run: /clips", hidden=True)
 
-# get_drop_metadata() - Logic to try to fuzzy-match user query to a drop
+# get_clip_metadata() - Logic to try to fuzzy-match user query to a clip
 # query[required] - String
 fuzz_threshold = 72
-def get_drop_metadata(query):
+def get_clip_metadata(query):
   query = strip_punctuation(query)
 
   top_score = [0,None]
 
-  for key in drop_data:
+  for key in clip_data:
     # If they nail a key directly, immediate return
     if (query == strip_punctuation(key)):
-      return drop_data.get(key)
+      return clip_data.get(key)
 
     # Otherwise do fuzzy-match on drop description
-    description = strip_punctuation(drop_data.get(key)["description"])
+    description = strip_punctuation(clip_data.get(key)["description"])
 
     ratio = fuzz.ratio(description, query)
     pratio = fuzz.partial_ratio(description, query)
@@ -113,7 +108,7 @@ def get_drop_metadata(query):
       top_score = [score, key]
 
   if (top_score[0] != 0):
-    return drop_data.get(top_score[1])
+    return clip_data.get(top_score[1])
   else:
     return False
 
@@ -141,7 +136,7 @@ async def check_timekeeper(ctx:SlashContext):
       # Check if we've notified the channel if there's a timeout active
       have_notified = last_record[1]
       if (have_notified == False):
-        await ctx.reply(f"{emojis['ohno']} Someone in the channel has already dropped too recently. Please wait a minute before another drop!", hidden=True)
+        await ctx.reply(f"{emojis['ohno']} Someone in the channel has already posted a clip too recently. Please wait a minute before another clip!", hidden=True)
         last_record[1] = True
       return False
 
@@ -159,12 +154,12 @@ punct_regex = r'[' + string.punctuation + ']'
 def strip_punctuation(string):
   return re.sub(punct_regex, '', string).lower().strip()
 
-def get_mp4(drop_metadata):
-  filename = drop_metadata['file']
+def get_mp4(clip_metadata):
+  filename = clip_metadata['file']
   if exists(filename):
     return filename
   else:
-    url = drop_metadata['url']
+    url = clip_metadata['url']
     r = requests.get(url, allow_redirects=True)
     open(filename, 'wb').write(r.content)
     return filename
