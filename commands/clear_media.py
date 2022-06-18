5f52c2d44c9ae1f67b6eb83082bae6dfb61e207b
Zane Mattingly <z@matting.ly>
2022-06-18 06:44:50 -0700

    clear_media command

---
+++ commands/clear_media.py
@@ -0,0 +1,41 @@
+from .common import *
+
+import os
+from glob import glob
+from pathlib import Path
+
+# Path to base bot directory
+base_path = Path(__file__).parent.parent
+
+# clear_media() - Entrypoint for !clear_media
+# message[required]: discord.Message
+# This function is the main entrypoint of the !clear_media command
+# This will remove all .mp4 files from the data/clips and data/drops directories
+# and allow the bot to re-load them as needed. Useful for debugging.
+async def clear_media(message:discord.Message):
+  drop_files = glob(os.path.join(base_path, 'data/drops/', '*.mp4'))
+  clip_files = glob(os.path.join(base_path, 'data/clips/', '*.mp4'))
+
+  errors = []
+  for file_path in [*drop_files, *clip_files]:
+    try:
+      os.remove(file_path)
+    except:
+      logger.debug(f"ERROR: Unable to remove file: {file_path}")
+      errors.append(file_path)
+
+  if errors:
+    embed = discord.Embed(
+      title="Error Deleting Files!",
+      description="\n".join(errors),
+      color=discord.Color.red()
+    )
+  else:
+    embed = discord.Embed(
+      title="Files Cleared Successfully!",
+      color=discord.Color.green()
+    )
+  
+  await message.channel.send(embed=embed)
+  
+  
