4186fc94010100595a8e58c7d9616d32a2c8723c
Zane Mattingly <z@matting.ly>
2022-06-20 00:16:14 -0700

    handle bot back-talk

--- commands/bot_autoresponse.py
+++ commands/bot_autoresponse.py
@@ -1,15 +1,39 @@
 from .common import *
 
-# Load up our list of complimentary adjectives
+# Load up our list of complimentary and condemnation adjectives
 with open('bot_affirmations.txt') as f:
-  bot_affirmations = f.read().splitlines() 
+  bot_affirmations = f.read().splitlines()
+with open('bot_condemnations.txt') as f:
+  bot_condmenations = f.read().splitlines() 
+
+logger.info(bot_condmenations)
 
 # bot_affirmations(message) - responds to bot compliments
 # message[required]: discord.Message
 async def handle_bot_affirmations(message:discord.Message):
+  message_content = message.content.lower()
+
+  for condemnation in bot_condmenations:
+    if f"{condemnation} bot" in message_content:
+      if not (re.match(fr"not a?\s?{condemnation} bot", message_content)):
+        await respond_to_sass(message)
+        return
+
   for affirmation in bot_affirmations:
-    if f"{affirmation} bot" in message.content.lower():
-      logger.info(f"The Bot received some {Fore.RED}love{Fore.RESET} from {Fore.LIGHTBLUE_EX}{message.author.display_name}{Fore.RESET}")
-      await message.add_reaction(EMOJI["love"])
-      await message.reply(random.choice(config["bot_responses"]), mention_author=True)
-      break
+    if f"{affirmation} bot" in message_content:
+      if re.match(fr"not a?\s?{affirmation} bot", message_content):
+        await respond_to_sass(message)
+        return
+      else:
+        await respond_to_praise(message)
+        return
+
+async def respond_to_sass(message:discord.Message):
+  logger.info(f"The Bot received some {Fore.YELLOW}sass{Fore.RESET} from {Fore.LIGHTBLUE_EX}{message.author.display_name}{Fore.RESET}")
+  await message.add_reaction(EMOJI["shocking"])
+  await message.reply(random.choice(config["bot_condemnation_responses"]), mention_author=True)
+
+async def respond_to_praise(message:discord.Message):
+  logger.info(f"The Bot received some {Fore.RED}love{Fore.RESET} from {Fore.LIGHTBLUE_EX}{message.author.display_name}{Fore.RESET}")
+  await message.add_reaction(EMOJI["shocking"])
+  await message.reply(random.choice(config["bot_affirmation_responses"]), mention_author=True)
