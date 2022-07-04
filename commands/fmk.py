1c8ebcf28bb256fd77ac1cfd9585dbe12bcae2d2
Zane Mattingly <z@matting.ly>
2022-07-04 14:29:35 -0700

    convert fmk to slash command

--- commands/fmk.py
+++ commands/fmk.py
@@ -1,14 +1,35 @@
 from common import *
+from utils.check_channel_access import access_check
 
-# fmk() - Entrypoint for !fmk command
-# message[required]: discord.Message
-# This function is the main entrypoint of the !fmk command
+# fmk() - Entrypoint for /fmk command
+# This function is the main entrypoint of the /fmk command
 # and will return a prompt with three random characters
-async def fmk(message:discord.Message):
+@bot.slash_command(
+  name="fmk",
+  description="Return 3 random Trek Characters as an FMK prompt!"
+)
+@commands.check(access_check)
+async def fmk(ctx:discord.ApplicationContext):
   f = open(config["commands"]["fmk"]["data"])
   fmk_characters = f.read().splitlines()
   f.close()
+
   choices = random.sample(fmk_characters, k=3)
-  msg = message.author.mention + ": Fuck Marry Kill (or Kiss) -- \n**{}, {}, {}**".format(choices[0], choices[1], choices[2])
-  await message.channel.send(msg)
-  
+  embed = discord.Embed(
+    title="Fuck Marry Kill (or Kiss)",
+    description="Who will it be?",
+    color=discord.Color.dark_gold()
+  )
+  embed.add_field(
+    name="Fuck",
+    value=choices[0]
+  )
+  embed.add_field(
+    name="Marry",
+    value=choices[1]
+  )
+  embed.add_field(
+    name="Kill (or Kiss)",
+    value=choices[2]
+  )
+  await ctx.respond(embed=embed)
