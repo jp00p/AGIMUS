ba6e731bafcc65c4f547b6d204050867a3ec4832
Jpop Wilson <asimovsghost@gmail.com>
2022-07-06 19:59:27 -0700

    CYA commit

--- commands/wordcloud.py
+++ commands/wordcloud.py
@@ -1,14 +1,16 @@
 from common import *
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
-
+# wordcloud() - Entrypoint for `/wordcloud` command
+# generates a wordcloud image based on users most-used words
+# also allows users to opt-in or out of logging their messages
 @bot.slash_command(
   name="wordcloud",
-  description="Show your own wordcloud!"
+  description="Show your own most popular words in a wordcloud!"
 )
 @option(
   name="enable_logging",
-  description="Set the bot to log your messages, or stop logging and erase all your messages",
+  description="Choose \"Yes\" to set the bot to log your messages, or \"No\" to stop logging and erase all your logged messages.",
   required=False,
   choices=[
     discord.OptionChoice(
@@ -22,8 +24,9 @@ from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
   ]
 )
 
-async def wordcloud(ctx, enable_logging):
+async def wordcloud(ctx:discord.ApplicationContext, enable_logging:str):
 
+  # handle logging toggle on/off
   if enable_logging == "Yes":
     db = getDB()
     query = db.cursor(dictionary=True)
@@ -33,7 +36,7 @@ async def wordcloud(ctx, enable_logging):
     db.commit()
     query.close()
     db.close()
-    await ctx.respond("Your messages will now be logged so we can generate a word cloud for you!")
+    await ctx.respond("Now logging your messages!  Once you have posted a few messages, check your wordcloud by typing `/wordcloud`", ephemeral=True)
     return
   
   if enable_logging == "No":
@@ -45,30 +48,49 @@ async def wordcloud(ctx, enable_logging):
     sql = "DELETE FROM message_history WHERE user_discord_id = %s"
     vals = (ctx.author.id,)
     query.execute(sql, vals)
+    deleted_row_count = query.rowcount
     query.close()
     db.commit()
     db.close()
-    await ctx.respond("Your messages will no longer be logged and all previous data has been removed.")
+    await ctx.respond(f"Your messages will no longer be logged and all previous data has been removed. ({deleted_row_count} messages deleted from the database)", ephemeral=True)
     return
 
+  user = get_user(ctx.author.id)
+
+  # if they have previously disabled logging
+  if user["log_messages"] != 1:
+    await ctx.respond("You do not have logging enabled. Use `/wordcloud enable_logging Yes` to start logging so we can generate a wordcloud for you.", ephemeral=True)
+    return
+
+  # get their message data
   user_details = get_wordcloud_text_for_user(ctx.author.id)
-  
+
+  # if they have no data yet
   if user_details is None:
-    await ctx.respond("No user data is available for you.  Try opting-in to the wordcloud by typing `/wordcloud enable_logging yes`")
+    await ctx.respond("No user data is available for you yet.", ephemeral=True)
     return
   
+  # mask image (combadge in this case, something else might work better)
   mask = np.array(Image.open("./images/cloud_masks/combadge_mask.png"))
+
+  # build wordcloud with magic of wordcloud lib
   wc = WordCloud(contour_color="#000044", contour_width=5, max_words=350, min_font_size=14, stopwords=STOPWORDS, mask=mask, font_path="./images/tng_font.ttf", background_color="black", mode="RGB", width=822, height=800, min_word_length=4).generate(user_details['full_message_text'])
+
+  # create PIL image 
   image = wc.to_image()
   image.save(f"./images/reports/wordcloud-{user_details['name']}.png")
+
+  # create discord image
   discord_image = discord.File(f"./images/reports/wordcloud-{user_details['name']}.png")
-  await ctx.respond(file=discord_image, ephemeral=False)
+
+  # send the image!
+  await ctx.respond(f"(Based on your last {user_details['num_messages']} messages)", file=discord_image, ephemeral=False)
 
 # get user's message history and return it in a dict
 def get_wordcloud_text_for_user(user_discord_id:int):
   db = getDB()
   query = db.cursor(dictionary=True)
-  sql = "SELECT message_history.user_discord_id, message_history.message_text as text, users.name FROM message_history LEFT JOIN users ON message_history.user_discord_id = users.discord_id WHERE message_history.user_discord_id = %s LIMIT 500"
+  sql = "SELECT message_history.user_discord_id, message_history.message_text as text, users.name FROM message_history LEFT JOIN users ON message_history.user_discord_id = users.discord_id WHERE message_history.user_discord_id = %s LIMIT 250"
   vals = (user_discord_id,)
   query.execute(sql, vals)
   results = query.fetchall()
--- handlers/save_message.py
+++ handlers/save_message.py
@@ -1,15 +1,19 @@
 from common import *
+from wordcloud import STOPWORDS
 
+# save_message_to_db() - saves a users message to the database after doing some cleanup on the message (strips emoji and converts to basic ascii, shuffles words in message)
 async def save_message_to_db(message:discord.Message):
   
   blocked_channels = get_channel_ids_list(config["handlers"]["save_message"]["blocked_channels"])
-  
   if message.channel.id in blocked_channels:
     return
   
   user = get_user(message.author.id)
   if user["log_messages"] == 1:
     message_content = message.content.encode("ascii", errors="ignore").decode().strip()
+    message_randomized = message_content.split(" ")
+    random.shuffle(message_randomized)
+    message_content = message_randomized.join(" ")
     remove_emoji = re.compile('<.*?>')
     message_content = re.sub(remove_emoji, '', message_content)
     if message_content.strip() == "":
