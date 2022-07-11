2a08a8d39a28fff92019a4a36b0ad7f05da1fd16
Jpop Wilson <asimovsghost@gmail.com>
2022-07-11 08:53:05 -0700

    xp improvements almost done

--- handlers/xp.py
+++ handlers/xp.py
@@ -1,4 +1,8 @@
+import math
+from time import sleep
+from numpy import block
 from common import *
+from commands.badges import give_user_badge
 
 # rainbow of colors to cycle through for the logs
 xp_colors = [
@@ -16,32 +20,35 @@ xp_colors = [
     Fore.LIGHTMAGENTA_EX
 ]
 current_color = 0
+blocked_channel_ids = get_channel_ids_list(config["handlers"]["xp"]["blocked_channels"])
+notification_channel_id = get_channel_id(config["handlers"]["xp"]["notification_channel"])
 
 # handle_message_xp(message) - calculates xp for a given message
 # message[required]: discord.Message
-async def handle_message_xp(message:discord.Message):
-    global current_color
-    
-    # we don't like bots round here
-    if message.author.bot:
+async def handle_message_xp(message:discord.Message):   
+    # we don't like bots round here, or some channels
+    if message.author.bot or message.channel.id in blocked_channel_ids:
       return
 
-    # Base XP to Grant
+    # base XP
     xp_amt = 0
-    # if the message is longer than 3 words +1 xp
+
+    # if the message is equal to or longer than 3 words +1 xp
     if len(message.content.split()) >= 3:
       xp_amt += 1
-      # if that message also has any of our emoji, +1 xp
+      
+      # if that message also has any of our server emoji, +1 xp
+      # case sensitive (cool != COOL)
       for e in config["all_emoji"]:
         if message.content.find(e) != -1:
           xp_amt += 1
           break
 
-    # if the message is longer than 33 words +1 xp
+    # if the message is longer than 33 words +1 more xp
     if len(message.content.split()) > 33:
       xp_amt += 1
 
-    # ...and 66, +1 more
+    # ...and 66, +1 more xp
     if len(message.content.split()) > 66:
       xp_amt += 1
 
@@ -50,15 +57,7 @@ async def handle_message_xp(message:discord.Message):
       xp_amt += 1 
 
     if xp_amt != 0:
-      msg_color = xp_colors[current_color]
-      star = f"{msg_color}{Style.BRIGHT}*{Style.NORMAL}{Fore.RESET}"
-      logger.info(f"{star} {msg_color}{message.author.display_name}{Fore.RESET} earns {msg_color}{xp_amt} XP{Fore.RESET} {star}")
-
-      increment_user_xp(message.author, xp_amt, "message", message.channel) # commit the xp gain to the db
-
-      current_color = current_color + 1
-      if current_color >= len(xp_colors):
-        current_color = 0
+      await increment_user_xp(message.author, xp_amt, "posted_message", message.channel) # commit the xp gain to the db
 
     # Handle Auto-Promotions
     promotion_roles_config = config["roles"]["promotion_roles"]
@@ -120,7 +119,7 @@ async def handle_rank_xp_promotion(message, xp):
     if role.name == ensign_role_name:
       ensign_role = role
 
-  user_xp = get_user_xp(message.author.id)
+  user_xp = get_user_xp(message.author.id).get("xp") # second element of tuple is the xp
 
   if cadet_role_name not in author_role_names:
     # if they don't have cadet yet and they are over the required xp, give it to them
@@ -134,26 +133,61 @@ async def handle_rank_xp_promotion(message, xp):
       logger.info(f"{Style.BRIGHT}{message.author.display_name}{Style.RESET_ALL} has been promoted to {Fore.GREEN}Ensign{Fore.RESET} via XP!")
 
 
+@bot.slash_command(
+  name="disable_xp",
+  description="Disable XP and stop receiving notifications from the bot"
+)
+@option(
+  name="disable",
+  description="Disable the notifications and stop participating in the XP game?",
+  required=True,
+  choices=[
+    discord.OptionChoice(
+      name="No",
+      value="no"
+    ),
+    discord.OptionChoice(
+      name="Yes",
+      value="yes"
+    )
+  ]
+)
+async def disable_xp(ctx:discord.ApplicationContext, disable:str):
+  user_selection = (disable == "yes")
+  db = getDB()
+  query = db.cursor()
+  sql = "UPDATE users SET xp_enabled = %s WHERE discord_id = %s"
+  vals = (not user_selection, ctx.user.id,)
+  query.execute(sql, vals)
+  db.commit()
+  query.close()
+  db.close()
+  response_msg = ""
+  if user_selection:
+    response_msg = "XP logging and notifications have been disabled for you!"
+    logger.info(f"{ctx.user.display_name} has disabled xp")
+  else:
+    response_msg = "XP logging and notifications have been re-enabled for you!"
+  await ctx.respond(response_msg, ephemeral=True)
+
 
 async def handle_react_xp(reaction:discord.Reaction, user:discord.User):
   # Check if this user has already reacted to this message with this emoji
 
-  if reaction.message.author.bot or user.bot:
+  if reaction.message.author.bot or user.bot or reaction.message.channel.id in blocked_channel_ids:
     return
 
   global current_color
-  msg_color = xp_colors[current_color]
-  star = f"{msg_color}{Style.BRIGHT}*{Style.NORMAL}{Fore.RESET}"
+  
 
   reaction_already_counted = check_react_history(reaction, user)
   if reaction_already_counted:
     #logger.info(f"{Fore.LIGHTRED_EX}{Style.BRIGHT}{user.display_name}{Style.RESET_ALL} has {Fore.RED}already reacted{Fore.RESET} to {Style.BRIGHT}Message #{reaction.message.id}{Style.RESET_ALL} with {Style.BRIGHT}{reaction.emoji.name}{Style.RESET_ALL} previously!")
     return
   
-  # If reaction hasn't been logged already, go ahead and do so and then award some XP!
-  logger.info(f"{star} {msg_color}{user.display_name}{Fore.RESET} earns {msg_color}1 XP{Fore.RESET} for reacting to a message! {star}")
+  
   log_react_history(reaction, user)
-  increment_user_xp(user, 1, "reacted", reaction.message.channel)
+  await increment_user_xp(user, 1, "added_reaction", reaction.message.channel)
 
   # Give the author some bonus XP if they've made a particularly reaction-worthy message!
   threshold_relevant_emojis = [
@@ -164,25 +198,71 @@ async def handle_react_xp(reaction:discord.Reaction, user:discord.User):
 
   xp_amt = 0
   if f"{reaction.emoji}" in threshold_relevant_emojis and reaction.count >= 5 and reaction.count < 10:
-    logger.info(f"{star} {msg_color}{reaction.message.author.display_name}{Fore.RESET} gets {msg_color}1 XP{Fore.RESET} for their message being reaction-worthy! {star}")
     xp_amt = 1
   
   if f"{reaction.emoji}" in threshold_relevant_emojis and reaction.count >= 10 and reaction.count < 20:
-    logger.info(f"{star} {msg_color}{reaction.message.author.display_name}{Fore.RESET} gets {msg_color}2 XP{Fore.RESET} for posting a very-well reacted-to message! {star}")
     xp_amt = 2
 
   if f"{reaction.emoji}" in threshold_relevant_emojis and reaction.count >= 20:
-    logger.info(f"{star} {msg_color}{reaction.message.author.display_name}{Fore.RESET} gets {msg_color}2 XP{Fore.RESET} for posting an {Style.BRIGHT} ULTRA REACTED-TO {Style.NORMAL}message! {star}")
-    #logger.info(f"{Back.LIGHTBLACK_EX}{Fore.CYAN}User {Style.BRIGHT}{reaction.message.author.display_name}{Style.RESET_ALL} gets {Style.BRIGHT}5 xp{Style.RESET_ALL} for their message being {Fore.CYAN}{Style.BRIGHT}**ULTRA**{Style.RESET_ALL} reaction-worthy!")
     xp_amt = 5
 
   if xp_amt > 0:
-    increment_user_xp(reaction.message.author, xp_amt, "reactions", reaction.message.channel)
+    await increment_user_xp(reaction.message.author, xp_amt, "got_reactions", reaction.message.channel)
 
-  current_color = current_color + 1
-  if current_color >= len(xp_colors):
-      current_color = 0
+  
 
+# calculate_xp_for_next_level(current_level)
+# current_level[required]: int
+# returns the amount of xp required to level up for the given level
+def calculate_xp_for_next_level(current_level:int):
+  return int( (current_level*69) + (current_level * current_level) - 1)
+
+# util function for debug
+def show_list_of_levels():
+  level_chart = ""
+  previous_xp_amt = 0
+  for i in range(101):
+    xp_required = calculate_xp_for_next_level(i)
+    amt_diff = xp_required - previous_xp_amt
+    previous_xp_amt = xp_required
+    level_chart += f"{i} - {xp_required} - ({amt_diff})\n"
+  logger.info(level_chart)
+
+# level_up_user(user, level)
+# user[required]:discord.User
+# level[required]:int
+# level up user to next level and give them a badge (in the DB)
+# also fires the send_level_up_message function
+async def level_up_user(user:discord.User, level:int):
+  logger.info(f"{Style.BRIGHT}{user.display_name} has reached level {level}!{Style.RESET_ALL}")
+  db = getDB()
+  query = db.cursor()
+  sql = "UPDATE users SET level = level + 1 WHERE discord_id = %s"
+  vals = (user.id,)
+  query.execute(sql, vals)
+  db.commit()
+  query.close()
+  db.close()
+  badge = give_user_badge(user.id)
+  await send_level_up_message(user, level, badge)
+
+# send_level_up_message(user, level, badge)
+# user[required]:discord.User
+# level[required]:int
+# badge[required]:str
+async def send_level_up_message(user:discord.User, level:int, badge:str):
+  logger.info(f"Sending level up message to {notification_channel_id}")
+  channel = bot.get_channel(notification_channel_id)
+  embed=discord.Embed(title="Level up!", description=f"{user.mention} has reached **level {level}** and earned a new badge!")
+  embed.set_thumbnail(url="https://i.imgur.com/Y8T9Yxa.jpg")
+  embed.set_footer(text="")
+  badge_name = badge.replace("_", " ").replace(".png", "")
+  embed.add_field(name="Badge name", value=badge_name)
+  embed_filename = str(user.id) + ".png"
+  discord_image = discord.File(fp=f"./images/badges/{badge}", filename=embed_filename)
+  sleep(0.1)
+  embed.set_image(url=f"attachment://{embed_filename}")
+  await channel.send(content=f"{user.mention} - Level up! See all your badges by typing `/badges` - disable this by typing `/disable_xp`", file=discord_image, embed=embed)
 
 
 # increment_user_xp(author, amt)
@@ -190,16 +270,37 @@ async def handle_react_xp(reaction:discord.Reaction, user:discord.User):
 # amt[required]: int
 # channel[required]: discord.Channel
 # This function will increment a users' XP and log the gain to the history
-def increment_user_xp(user, amt, reason, channel):
+async def increment_user_xp(user, amt, reason, channel):
+  global current_color
+  msg_color = xp_colors[current_color]
+  
+      
+  star = f"{msg_color}{Style.BRIGHT}*{Style.NORMAL}{Fore.RESET}"
   db = getDB()
   query = db.cursor()
-  sql = "UPDATE users SET xp = xp + %s, name = %s WHERE discord_id = %s"
+  sql = "UPDATE users SET xp = xp + %s, name = %s WHERE discord_id = %s AND xp_enabled = 1"
   vals = (amt, user.display_name, user.id)
   query.execute(sql, vals)
+  updated = query.rowcount
   db.commit()
   query.close()
   db.close()
-  log_xp_history(user.id, amt, channel.id, reason)
+  if updated > 0:
+    log_xp_history(user.id, amt, channel.id, reason)
+    # If reaction hasn't been logged already, go ahead and do so and then award some XP!
+    logger.info(f"{star} {msg_color}{user.display_name}{Fore.RESET} earns {msg_color}{amt} XP{Fore.RESET} for {reason}! {star}")
+    current_color = current_color + 1
+    if current_color >= len(xp_colors):
+        current_color = 0
+    user_xp_data = get_user_xp(user.id)
+    user_xp = user_xp_data["xp"]
+    next_level_xp = calculate_xp_for_next_level(user_xp_data["level"])
+    #logger.info(f'User XP: {user_xp} User level: {user_xp_data["level"]} Next level XP: {next_level_xp}')
+    if user_xp >= next_level_xp:
+      try:
+        await level_up_user(user, user_xp_data["level"]+1)
+      except Exception as e:
+        logger.info(f"Error trying to level up user: {e}")
 
 # get_user_xp(discord_id)
 # discord_id[required]: int
@@ -207,14 +308,14 @@ def increment_user_xp(user, amt, reason, channel):
 def get_user_xp(discord_id):
   db = getDB()
   query = db.cursor()
-  sql = "SELECT xp FROM users WHERE discord_id = %s"
+  sql = "SELECT level, xp FROM users WHERE discord_id = %s"
   vals = (discord_id,)
   query.execute(sql, vals)
   user_xp = query.fetchone()
   db.commit()
   query.close()
   db.close()
-  return user_xp[0]
+  return { "level": user_xp[0], "xp" : user_xp[1] }
 
 
 def check_react_history(reaction:discord.Reaction, user:discord.User):
--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 from common import *
 
 # Slash Commands
+from commands.badges import badges
 from commands.dustbuster import dustbuster
 from commands.fmk import fmk
 from commands.help import help
@@ -55,7 +56,7 @@ from handlers.bot_autoresponse import handle_bot_affirmations
 from handlers.save_message import save_message_to_db
 from handlers.server_logs import *
 from handlers.starboard import get_all_starboard_posts, handle_starboard_reactions
-from handlers.xp import handle_message_xp, handle_react_xp
+from handlers.xp import handle_message_xp, handle_react_xp, calculate_xp_for_next_level
 
 # Tasks
 from tasks.bingbong import bingbong_task
@@ -85,7 +86,7 @@ async def on_message(message:discord.Message):
     await bot.process_commands(message)
   except BaseException as e:
     logger.info(f"{Fore.RED}<! ERROR: Encountered error in process_commands !> {e}{Fore.RESET}")
-    logger.info(traceback.format_exc())    
+    logger.info(traceback.format_exc())
 
   # message logging
   try:
@@ -160,44 +161,52 @@ async def process_command(message:discord.Message):
 
 @bot.event
 async def on_ready():
-  logger.info(f"{Back.LIGHTRED_EX}{Fore.LIGHTWHITE_EX}LOGGED IN AS {bot.user}{Fore.RESET}{Back.RESET}")
+  try:
+    logger.info(f"{Back.LIGHTRED_EX}{Fore.LIGHTWHITE_EX}LOGGED IN AS {bot.user}{Fore.RESET}{Back.RESET}")
 
-  # We can handle this better late with an Emoji class helper, but for now just create a dict
-  # Use EMOJI dict from common
-  for e in bot.emojis:
-    EMOJIS[e.name] = e
+    # We can handle this better late with an Emoji class helper, but for now just create a dict
+    # Use EMOJI dict from common
+    for e in bot.emojis:
+      EMOJIS[e.name] = e
 
-  global ALL_STARBOARD_POSTS
-  ALL_USERS = get_all_users()
-  ALL_STARBOARD_POSTS = get_all_starboard_posts()
-  number_of_starboard_posts = len(ALL_STARBOARD_POSTS)
-  for emoji in bot.emojis:
-    config["all_emoji"].append(emoji.name)
-  #logger.info(client.emojis) -- save this for later, surely we can do something with all these emojis
-  #logger.info(f"ALL_STARBOARD_POSTS:\n{ALL_STARBOARD_POSTS}")
+    global ALL_STARBOARD_POSTS
+    ALL_USERS = get_all_users()
+    ALL_STARBOARD_POSTS = get_all_starboard_posts()
+    number_of_starboard_posts = len(ALL_STARBOARD_POSTS)
+    for emoji in bot.emojis:
+      config["all_emoji"].append(emoji.name)
+    #logger.info(client.emojis) -- save this for later, surely we can do something with all these emojis
+    #logger.info(f"ALL_STARBOARD_POSTS:\n{ALL_STARBOARD_POSTS}")
 
-  # Print AGIMUS ANSI Art
-  agimus_ascii = []
-  with open('data/ascii/agimus.txt') as f:
-    agimus_ascii = f.readlines()
-  logger.info(''.join(agimus_ascii))
-  logger.info(f"{Fore.LIGHTMAGENTA_EX}BOT IS ONLINE AND READY FOR COMMANDS!{Fore.RESET}")
-  logger.info(f"{Fore.LIGHTRED_EX}CURRENT NUMBER OF STARBOARD POSTS:{Fore.RESET}{Style.BRIGHT} {Fore.BLUE}{number_of_starboard_posts}{Fore.RESET}{Style.RESET_ALL}")
+    # Print AGIMUS ANSI Art
+    agimus_ascii = []
+    with open('data/ascii/agimus.txt') as f:
+      agimus_ascii = f.readlines()
+    logger.info(''.join(agimus_ascii))
+    logger.info(f"{Fore.LIGHTMAGENTA_EX}BOT IS ONLINE AND READY FOR COMMANDS!{Fore.RESET}")
+    logger.info(f"{Fore.LIGHTRED_EX}CURRENT NUMBER OF STARBOARD POSTS:{Fore.RESET}{Style.BRIGHT} {Fore.BLUE}{number_of_starboard_posts}{Fore.RESET}{Style.RESET_ALL}")      
 
-  # Set a fun random presence
-  random_presences = [
-    { 'name': "PRAISE THE FOUNDERS", 'type': discord.ActivityType.listening },
-    { 'name': "The Greatest Generation", 'type': discord.ActivityType.listening },
-    { 'name': "The Greatest Discovery", 'type': discord.ActivityType.listening },
-    { 'name': "A Nice Game of Chess", 'type': discord.ActivityType.playing },
-    { 'name': "Thermonuclear War", 'type': discord.ActivityType.playing },
-    { 'name': "Dials", 'type': discord.ActivityType.playing },
-    { 'name': "The Stream At Home", 'type': discord.ActivityType.watching },
-    { 'name': "and waiting...", 'type': discord.ActivityType.watching },
-    { 'name': "Terminator 2: Judgement Day", 'type': discord.ActivityType.watching }
-  ]
-  selected_presence = random.choice(random_presences)
-  await bot.change_presence(status=discord.Status.online, activity=discord.Activity(name=selected_presence['name'], type=selected_presence['type']))
+    # generate local files if the list doesn't exist already
+    if not os.path.exists("./local-channel-list.json"):
+      generate_local_channel_list(bot)
+
+    # Set a fun random presence
+    random_presences = [
+      { 'name': "PRAISE THE FOUNDERS", 'type': discord.ActivityType.listening },
+      { 'name': "The Greatest Generation", 'type': discord.ActivityType.listening },
+      { 'name': "The Greatest Discovery", 'type': discord.ActivityType.listening },
+      { 'name': "A Nice Game of Chess", 'type': discord.ActivityType.playing },
+      { 'name': "Thermonuclear War", 'type': discord.ActivityType.playing },
+      { 'name': "Dials", 'type': discord.ActivityType.playing },
+      { 'name': "The Stream At Home", 'type': discord.ActivityType.watching },
+      { 'name': "and waiting...", 'type': discord.ActivityType.watching },
+      { 'name': "Terminator 2: Judgement Day", 'type': discord.ActivityType.watching }
+    ]
+    selected_presence = random.choice(random_presences)
+    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(name=selected_presence['name'], type=selected_presence['type']))
+  except Exception as e:
+    logger.info(f"Error in on_ready: {e}")
+    logger.info(traceback.format_exc())
 
 
 # listen to reactions
