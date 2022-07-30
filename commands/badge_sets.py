c19b01307f2313601d35dcba71c3a1556e038709
Zane Mattingly <z@matting.ly>
2022-07-30 09:12:33 -0700

    working badge sets

--- commands/badge_sets.py
+++ commands/badge_sets.py
@@ -7,9 +7,25 @@ from common import *
 # from utils.check_channel_access import access_check
 
 # AUTOCOMPLETE
-async def autocomplete_affiliations(ctx:discord.AutocompleteContext):
-  affiliations = db_get_all_affiliations()
-  return [result for result in affiliations if ctx.value.lower() in result.lower()]
+async def autocomplete_selections(ctx:discord.AutocompleteContext):
+  category = ctx.options["category"]
+
+  selections = []
+
+  if category == 'affiliation':
+    selections = db_get_all_affiliations()
+    # return [result for result in affiliations if ctx.value.lower() in result.lower()]
+  elif category == 'franchise':
+    selections = db_get_all_franchises()
+    # return [result for result in franchises if ctx.value.lower() in result.lower()]
+  elif category == 'time_period':
+    selections = db_get_all_time_periods()
+    # return [result for result in time_periods if ctx.value.lower() in result.lower()]
+  elif category == 'type':
+    selections = db_get_all_types()
+    # return [result for result in types if ctx.value.lower() in result.lower()]
+
+  return [result for result in selections if ctx.value.lower() in result.lower()]
 
 # COMMAND
 @bot.slash_command(
@@ -17,41 +33,211 @@ async def autocomplete_affiliations(ctx:discord.AutocompleteContext):
   description="Show off sets of your badges!"
 )
 @option(
-  name="affiliation",
-  description="Which faction?",
+  name="category",
+  description="Which category of set?",
   required=True,
-  autocomplete=autocomplete_affiliations
+  choices=[
+    discord.OptionChoice(
+      name="Affiliation",
+      value="affiliation"
+    ),
+    discord.OptionChoice(
+      name="Franchise",
+      value="franchise"
+    ),
+    discord.OptionChoice(
+      name="Time Period",
+      value="time_period"
+    ),
+    discord.OptionChoice(
+      name="Type",
+      value="type"
+    )
+  ]
 )
-async def badge_sets(ctx:discord.ApplicationContext, affiliation:str):
-  await ctx.defer()
+@option(
+  name="selection",
+  description="Which one?",
+  required=True,
+  autocomplete=autocomplete_selections
+)
+async def badge_sets(ctx:discord.ApplicationContext, category:str, selection:str):
+  await ctx.defer(ephemeral=True)
 
-  affiliations = db_get_all_affiliations()
-  if affiliation not in affiliations:
-    await ctx.followup.send("Your entry was not in the list of affiliations!", ephemeral=True)
-    return
+  user_set_badges = []
+  all_set_badges = []
 
-  users_affiliation_badges = db_get_badges_user_has_from_affiliation(ctx.author.id, affiliation)
-  # logger.info(pprint(users_affiliation_badges))
+  if category == 'affiliation':
+    affiliations = db_get_all_affiliations()
+    if selection not in affiliations:
+      await ctx.followup.send("Your entry was not in the list of Affiliations!", ephemeral=True)
+      return
 
-  all_affiliation_badges = db_get_all_affiliation_badges(affiliation)
-  # logger.info(pprint(all_affiliation_badges))
+    user_set_badges = db_get_badges_user_has_from_affiliation(ctx.author.id, selection)
+    all_set_badges = db_get_all_affiliation_badges(selection)
+
+  elif category == 'franchise':
+    franchises = db_get_all_franchises()
+    if selection not in franchises:
+      await ctx.followup.send("Your entry was not in the list of Franchises!", ephemeral=True)
+      return
+
+    user_set_badges = db_get_badges_user_has_from_franchise(ctx.author.id, selection)
+    all_set_badges = db_get_all_franchise_badges(selection)
+
+  elif category == 'time_period':
+    time_periods = db_get_all_time_periods()
+    if selection not in time_periods:
+      await ctx.followup.send("Your entry was not in the list of Time Periods!", ephemeral=True)
+      return
+
+    user_set_badges = db_get_badges_user_has_from_time_period(ctx.author.id, selection)
+    all_set_badges = db_get_all_time_period_badges(selection)
+
+  elif category == 'type':
+    types = db_get_all_types()
+    if selection not in types:
+      await ctx.followup.send("Your entry was not in the list of Types!", ephemeral=True)
+      return
+
+    user_set_badges = db_get_badges_user_has_from_type(ctx.author.id, selection)
+    all_set_badges = db_get_all_type_badges(selection)
+
 
   set = []
-  for badge in all_affiliation_badges:
-    badge_filename = badge.replace(" ", "_")#.replace("'", "’").replace("’")
+  for badge in all_set_badges:
+    badge_filename = badge.replace(" ", "_").replace("/", "-")
     record = {
       'badge_name': badge,
       'badge_filename': f"{badge_filename}.png",
-      'in_user_collection': badge in users_affiliation_badges
+      'in_user_collection': badge in user_set_badges
     }
     set.append(record)
 
-  # logger.info(pprint(set))
+  category_title = category.replace("_", " ").title()
 
-  set_image = generate_badge_set_showcase_for_user(ctx.author, set, affiliation)
+  set_image = generate_badge_set_showcase_for_user(ctx.author, set, selection, category_title)
 
   await ctx.followup.send(file=set_image, ephemeral=False)
 
+
+# big expensive function that generates a nice grid of images for the user
+# returns discord.file
+def generate_badge_set_showcase_for_user(user:discord.User, badge_set, selection, category_title):
+  total_user_badges = db_get_badge_count_for_user(user.id)
+
+  text_wrapper = textwrap.TextWrapper(width=22)
+  # badge_list = get_user_badges(user.id)
+  title_font = ImageFont.truetype("fonts/lcars3.ttf", 110)
+  collected_font = ImageFont.truetype("fonts/lcars3.ttf", 70)
+  total_font = ImageFont.truetype("fonts/lcars3.ttf", 54)
+  badge_font = ImageFont.truetype("fonts/context_bold.ttf", 28)
+
+  badge_size = 200
+  badge_padding = 40
+  badge_margin = 10
+  badge_slot_size = badge_size + (badge_padding * 2) # size of badge slot size (must be square!)
+  badges_per_row = 6
+
+  base_width = 1890
+  base_header_height = 530
+  base_row_height = 290
+  base_footer_height = 200
+
+  number_of_rows = math.ceil((len(badge_set) / badges_per_row)) - 1
+
+  base_height = base_header_height + (base_row_height * number_of_rows) + base_footer_height
+  # base_height = math.ceil((len(badge_set) / badges_per_row)) * (badge_slot_size + badge_margin)
+
+  # create base image to paste all badges on to
+  badge_base_image = Image.new("RGBA", (base_width, base_height), (0, 0, 0))
+  base_header_image = Image.open("./images/templates/badge_sets/badge_set_header.png")
+  base_row_image = Image.open("./images/templates/badge_sets/badge_set_row.png")
+  base_footer_image = Image.open("./images/templates/badge_sets/badge_set_footer.png")
+
+  # Start image with header
+  badge_base_image.paste(base_header_image, (0, 0))
+
+  # Stamp rows (if needed, header includes first row)
+  base_current_y = base_header_height
+  for i in range(number_of_rows):
+    badge_base_image.paste(base_row_image, (0, base_current_y))
+    base_current_y += base_row_height
+
+  # Stamp footer
+  badge_base_image.paste(base_footer_image, (0, base_current_y))
+
+  draw = ImageDraw.Draw(badge_base_image)
+
+  draw.text( (100, 65), f"{user.display_name.encode('ascii', errors='ignore').decode().strip()}'s Badge Set: {category_title} - {selection}", fill="#8DB9B5", font=title_font, align="left")
+  draw.text( (590, base_height - 113), f"{len([b for b in badge_set if b['in_user_collection']])} OF {len(badge_set)}", fill="#47AAB1", font=collected_font, align="left")
+  draw.text( (32, base_height - 90), f"{total_user_badges}", fill="#47AAB1", font=total_font, align="left")
+
+  start_x = 100
+  current_x = start_x
+  current_y = 245
+  counter = 0
+
+  for badge_record in badge_set:
+    badge_border_color = "#47AAB1"
+    badge_text_color = "white"
+    if not badge_record['in_user_collection']:
+      badge_border_color = "#575757"
+      badge_text_color = "#888888"
+
+    # slot
+    s = Image.new("RGBA", (badge_slot_size, badge_slot_size), (0, 0, 0, 0))
+    badge_draw = ImageDraw.Draw(s)
+    badge_draw.rounded_rectangle( (0, 0, badge_slot_size, badge_slot_size), fill="#000000", outline=badge_border_color, width=4, radius=32 )
+
+    # badge
+    b = Image.open(f"./images/badges/{badge_record['badge_filename']}").convert("RGBA")
+    if not badge_record['in_user_collection']:
+      # Create a mask layer to apply a 1/4th opacity to
+      b2 = b.copy()
+      b2.putalpha(64)
+      b.paste(b2, b)
+    b = b.resize((190, 190))
+
+    w, h = b.size # badge size
+    offset_x = min(0, (badge_size+badge_padding)-w) # center badge x
+    offset_y = 5
+    badge_name = text_wrapper.wrap(badge_record['badge_name'])
+    wrapped_badge_name = ""
+    for i in badge_name[:-1]:
+      wrapped_badge_name = wrapped_badge_name + i + "\n"
+    wrapped_badge_name += badge_name[-1]
+    # add badge to slot
+    s.paste(b, (badge_padding+offset_x+4, offset_y), b)
+    badge_draw.text( (int(badge_slot_size/2), 222), f"{wrapped_badge_name}", fill=badge_text_color, font=badge_font, anchor="mm", align="center")
+
+    # add slot to base image
+    badge_base_image.paste(s, (current_x, current_y), s)
+
+    current_x += badge_slot_size + badge_margin
+    counter += 1
+
+    if counter % badges_per_row == 0:
+      # typewriter sound effects:
+      current_x = start_x # ding!
+      current_y += badge_slot_size + badge_margin # ka-chunk
+      counter = 0 #...
+
+  badge_set_filepath = f"./images/profiles/badge_set_{user.id}_{selection.lower().replace(' ', '-')}.png"
+  badge_base_image.save(badge_set_filepath)
+
+  while True:
+    time.sleep(0.05)
+    if os.path.isfile(badge_set_filepath):
+      break
+
+  discord_image = discord.File(badge_set_filepath)
+  return discord_image
+
+
+#
+# AFFILIATIONS
+#
 
 def db_get_all_affiliations():
   db = getDB()
@@ -63,7 +249,7 @@ def db_get_all_affiliations():
   query.close()
   db.close()
 
-  affiliations = [r['affiliation_name'] for r in rows]
+  affiliations = [r['affiliation_name'] for r in rows if r['affiliation_name'] is not None]
   affiliations.sort()
 
   return affiliations
@@ -113,98 +299,209 @@ def db_get_badges_user_has_from_affiliation(user_id, affiliation):
 
   return user_badges
 
+#
+# FRANCHISES
+#
+def db_get_all_franchises():
+  db = getDB()
+  query = db.cursor(dictionary=True)
+  sql = "SELECT distinct(franchise) FROM badge_info"
+  query.execute(sql)
+  rows = query.fetchall()
+  db.commit()
+  query.close()
+  db.close()
 
+  franchises = [r['franchise'] for r in rows if r['franchise'] is not None]
+  franchises.sort()
 
-# XXX REFACTOR ME AND PUT ME IN UTIL
-# big expensive function that generates a nice grid of images for the user
-# returns discord.file
-def generate_badge_set_showcase_for_user(user:discord.User, badge_set, affiliation):
-  text_wrapper = textwrap.TextWrapper(width=22)
-  # badge_list = get_user_badges(user.id)
-  title_font = ImageFont.truetype("fonts/tng_credits.ttf", 68)
-  credits_font = ImageFont.truetype("fonts/tng_credits.ttf", 42)
-  badge_font = ImageFont.truetype("fonts/context_bold.ttf", 28)
+  return franchises
 
-  badge_size = 200
-  badge_padding = 40
-  badge_margin = 10
-  badge_slot_size = badge_size + (badge_padding * 2) # size of badge slot size (must be square!)
-  badges_per_row = 6
+def db_get_all_franchise_badges(franchise):
+  db = getDB()
+  query = db.cursor(dictionary=True)
+  sql = '''
+    SELECT badge_name FROM badge_info b_i
+      WHERE franchise = %s
+  '''
+  vals = (franchise,)
+  query.execute(sql, vals)
+  rows = query.fetchall()
+  db.commit()
+  query.close()
+  db.close()
 
-  base_width = (badge_slot_size+badge_margin) * badges_per_row
-  base_height = math.ceil((len(badge_set) / badges_per_row)) * (badge_slot_size + badge_margin)
+  franchise_badges = [r['badge_name'] for r in rows]
+  franchise_badges.sort()
 
-  header_height = badge_size
-  image_padding = 50
+  return franchise_badges
 
-  # create base image to paste all badges on to
-  # what if they have 900 badges?
-  badge_base_image = Image.new("RGBA", (base_width+(image_padding*2), base_height+header_height+(image_padding*2)), (200, 200, 200))
-  badge_bg_image = Image.open("./images/stars/" + random.choice(os.listdir("./images/stars/")))
+def db_get_badges_user_has_from_franchise(user_id, franchise):
+  db = getDB()
+  query = db.cursor(dictionary=True)
+  sql = '''
+    SELECT b_i.badge_name FROM badges b
+      JOIN badge_info AS b_i
+        ON b.badge_name = b_i.badge_filename
+      WHERE b.user_discord_id = %s
+        AND b_i.franchise = %s
+  '''
+  vals = (user_id, franchise)
+  query.execute(sql, vals)
+  rows = query.fetchall()
+  db.commit()
+  query.close()
+  db.close()
 
-  base_w, base_h = badge_base_image.size
-  bg_w, bg_h = badge_bg_image.size
+  user_badges = [r['badge_name'] for r in rows]
+  user_badges.sort()
 
-  for i in range(0, base_w, bg_w):
-    for j in range(0, base_h, bg_h):
-      badge_base_image.paste(badge_bg_image, (i, j))
+  return user_badges
 
-  draw = ImageDraw.Draw(badge_base_image)
+#
+# TIME PERIODS
+#
+def db_get_all_time_periods():
+  db = getDB()
+  query = db.cursor(dictionary=True)
+  sql = "SELECT distinct(time_period) FROM badge_info"
+  query.execute(sql)
+  rows = query.fetchall()
+  db.commit()
+  query.close()
+  db.close()
 
-  draw.text( (base_width/2, 100), f"{user.display_name.encode('ascii', errors='ignore').decode().strip()}'s {affiliation } badges", fill="white", font=title_font, anchor="mm", align="center")
-  draw.text( (base_width/2, base_h-50), f"Collected {len([b for b in badge_set if b['in_user_collection']])} of {len(badge_set)}", fill="white", font=credits_font, anchor="mm", align="center",stroke_width=2,stroke_fill="#000000")
+  time_periods = [r['time_period'] for r in rows if r['time_period'] is not None]
+  time_periods.sort(key=_time_period_sort)
 
-  start_x = image_padding
-  current_x = start_x
-  current_y = header_height
-  counter = 0
-  badge_border_color = random.choice(["#774466", "#6688CC", "#BB4411", "#0011EE"])
+  return time_periods
 
-  for badge_record in badge_set:
-    # todo: create slot border/stuff, lcars stuff
+# We may be dealing with time periods before 1000,
+# so tack on a 0 prefix for these for proper sorting
+def _time_period_sort(time_period):
+  if len(time_period) == 4:
+    return f"0{time_period}"
+  else:
+    return time_period
 
-    # slot
-    s = Image.new("RGBA", (badge_slot_size, badge_slot_size), (0, 0, 0, 0))
-    badge_draw = ImageDraw.Draw(s)
-    badge_draw.rounded_rectangle( (0, 0, badge_slot_size, badge_slot_size), fill="#000000", outline=badge_border_color, width=4, radius=32 )
+def db_get_all_time_period_badges(time_period):
+  db = getDB()
+  query = db.cursor(dictionary=True)
+  sql = '''
+    SELECT badge_name FROM badge_info b_i
+      WHERE time_period = %s
+  '''
+  vals = (time_period,)
+  query.execute(sql, vals)
+  rows = query.fetchall()
+  db.commit()
+  query.close()
+  db.close()
 
-    # badge
-    b = Image.open(f"./images/badges/{badge_record['badge_filename']}").convert("RGBA")
-    if not badge_record['in_user_collection']:
-      b = ImageOps.grayscale(b)
-    b = b.resize((190, 190))
+  time_period_badges = [r['badge_name'] for r in rows]
+  time_period_badges.sort()
 
-    w, h = b.size # badge size
-    offset_x = min(0, (badge_size+badge_padding)-w) # center badge x
-    offset_y = 5
-    badge_name = text_wrapper.wrap(badge_record['badge_name'])
-    wrapped_badge_name = ""
-    for i in badge_name[:-1]:
-      wrapped_badge_name = wrapped_badge_name + i + "\n"
-    wrapped_badge_name += badge_name[-1]
-    # add badge to slot
-    s.paste(b, (badge_padding+offset_x+4, offset_y), b)
-    badge_draw.text( (int(badge_slot_size/2), 222), f"{wrapped_badge_name}", fill="white", font=badge_font, anchor="mm", align="center")
+  return time_period_badges
 
-    # add slot to base image
-    badge_base_image.paste(s, (current_x, current_y), s)
+def db_get_badges_user_has_from_time_period(user_id, time_period):
+  db = getDB()
+  query = db.cursor(dictionary=True)
+  sql = '''
+    SELECT b_i.badge_name FROM badges b
+      JOIN badge_info AS b_i
+        ON b.badge_name = b_i.badge_filename
+      WHERE b.user_discord_id = %s
+        AND b_i.time_period = %s
+  '''
+  vals = (user_id, time_period)
+  query.execute(sql, vals)
+  rows = query.fetchall()
+  db.commit()
+  query.close()
+  db.close()
 
-    current_x += badge_slot_size + badge_margin
-    counter += 1
+  user_badges = [r['badge_name'] for r in rows]
+  user_badges.sort()
 
-    if counter % badges_per_row == 0:
-      # typewriter sound effects:
-      current_x = start_x # ding!
-      current_y += badge_slot_size + badge_margin # ka-chunk
-      counter = 0 #...
+  return user_badges
 
-  badge_set_filepath = f"./images/profiles/badge_set_{user.id}_{affiliation.lower().replace(' ', '-')}.png"
-  badge_base_image.save(badge_set_filepath)
+#
+# TYPES
+#
+def db_get_all_types():
+  db = getDB()
+  query = db.cursor(dictionary=True)
+  sql = "SELECT distinct(type_name) FROM badge_type"
+  query.execute(sql)
+  rows = query.fetchall()
+  db.commit()
+  query.close()
+  db.close()
 
-  while True:
-    time.sleep(0.05)
-    if os.path.isfile(badge_set_filepath):
-      break
+  types = [r['type_name'] for r in rows if r['type_name'] is not None]
+  types.sort()
 
-  discord_image = discord.File(badge_set_filepath)
-  return discord_image
+  return types
+
+def db_get_all_type_badges(type):
+  db = getDB()
+  query = db.cursor(dictionary=True)
+  sql = '''
+    SELECT badge_name FROM badge_info b_i
+      JOIN badge_type AS b_t
+        ON b_i.id = b_t.badge_id
+      WHERE b_t.type_name = %s
+  '''
+  vals = (type,)
+  query.execute(sql, vals)
+  rows = query.fetchall()
+  db.commit()
+  query.close()
+  db.close()
+
+  type_badges = [r['badge_name'] for r in rows]
+  type_badges.sort()
+
+  return type_badges
+
+def db_get_badges_user_has_from_type(user_id, type):
+  db = getDB()
+  query = db.cursor(dictionary=True)
+  sql = '''
+    SELECT b_i.badge_name FROM badges b
+      JOIN badge_info AS b_i
+        ON b.badge_name = b_i.badge_filename
+      JOIN badge_type AS b_t
+        ON b_i.id = b_t.badge_id
+      WHERE b.user_discord_id = %s
+        AND b_t.type_name = %s
+  '''
+  vals = (user_id, type)
+  query.execute(sql, vals)
+  rows = query.fetchall()
+  db.commit()
+  query.close()
+  db.close()
+
+  user_badges = [r['badge_name'] for r in rows]
+  user_badges.sort()
+
+  return user_badges
+
+#
+# GENERAL
+#
+def db_get_badge_count_for_user(user_id):
+  db = getDB()
+  query = db.cursor(dictionary=True)
+  sql = '''
+    SELECT count(*) FROM badges WHERE user_discord_id = %s
+  '''
+  vals = (user_id,)
+  query.execute(sql, vals)
+  result = query.fetchone()
+  db.commit()
+  query.close()
+  db.close()
+
+  return result['count(*)']
