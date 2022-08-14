8dca8e49027212f227e5ed024e43b939344acb6c
Jpop Wilson <asimovsghost@gmail.com>
2022-08-14 15:14:07 -0700

    fix migrate

--- migrations/v1.5.2.sql
+++ migrations/v1.5.2.sql
@@ -1,19 +1,19 @@
 INSERT INTO badge_info (badge_name, badge_filename, badge_url, quadrant, time_period, franchise, reference) VALUES ("Captain Picard Day", "Captain_Picard_Day.png", "https://drunkshimoda.com/", "Alpha", "2300s", "The USS Hood", "https://memory-alpha.fandom.com/wiki/Captain_Picard_Day");
-INSERT INTO badges (user_discord_id, badge_name) VALUES 339929776473243659, "Captain_Picard_Day.png";
-INSERT INTO badges (user_discord_id, badge_name) VALUES 217444623574892565, "Captain_Picard_Day.png";
-INSERT INTO badges (user_discord_id, badge_name) VALUES 875088871354937375, "Captain_Picard_Day.png";
-INSERT INTO badges (user_discord_id, badge_name) VALUES 874860184747524116, "Captain_Picard_Day.png";
-INSERT INTO badges (user_discord_id, badge_name) VALUES 874829796717256765, "Captain_Picard_Day.png";
-INSERT INTO badges (user_discord_id, badge_name) VALUES 873347065269354517, "Captain_Picard_Day.png";
-INSERT INTO badges (user_discord_id, badge_name) VALUES 837384857737887764, "Captain_Picard_Day.png";
+INSERT INTO badges (user_discord_id, badge_name) VALUES (339929776473243659, "Captain_Picard_Day.png");
+INSERT INTO badges (user_discord_id, badge_name) VALUES (217444623574892565, "Captain_Picard_Day.png");
+INSERT INTO badges (user_discord_id, badge_name) VALUES (875088871354937375, "Captain_Picard_Day.png");
+INSERT INTO badges (user_discord_id, badge_name) VALUES (874860184747524116, "Captain_Picard_Day.png");
+INSERT INTO badges (user_discord_id, badge_name) VALUES (874829796717256765, "Captain_Picard_Day.png");
+INSERT INTO badges (user_discord_id, badge_name) VALUES (873347065269354517, "Captain_Picard_Day.png");
+INSERT INTO badges (user_discord_id, badge_name) VALUES (837384857737887764, "Captain_Picard_Day.png");
 
-INSERT INTO badges (user_discord_id, badge_name) VALUES 382442396295888896, "Captain_Picard_Day.png";
-INSERT INTO badges (user_discord_id, badge_name) VALUES 875370460454486096, "Captain_Picard_Day.png";
+INSERT INTO badges (user_discord_id, badge_name) VALUES (382442396295888896, "Captain_Picard_Day.png");
+INSERT INTO badges (user_discord_id, badge_name) VALUES (875370460454486096, "Captain_Picard_Day.png");
 
-INSERT INTO badges (user_discord_id, badge_name) VALUES 399251290372440066, "Captain_Picard_Day.png";
-INSERT INTO badges (user_discord_id, badge_name) VALUES 875573307666214942, "Captain_Picard_Day.png";
-INSERT INTO badges (user_discord_id, badge_name) VALUES 704531275729469481, "Captain_Picard_Day.png";
+INSERT INTO badges (user_discord_id, badge_name) VALUES (399251290372440066, "Captain_Picard_Day.png");
+INSERT INTO badges (user_discord_id, badge_name) VALUES (875573307666214942, "Captain_Picard_Day.png");
+INSERT INTO badges (user_discord_id, badge_name) VALUES (704531275729469481, "Captain_Picard_Day.png");
 
-INSERT INTO badges (user_discord_id, badge_name) VALUES 382618661376229377, "Captain_Picard_Day.png";
-INSERT INTO badges (user_discord_id, badge_name) VALUES 875936903801737247, "Captain_Picard_Day.png";
-INSERT INTO badges (user_discord_id, badge_name) VALUES 873883287096033331, "Captain_Picard_Day.png";
+INSERT INTO badges (user_discord_id, badge_name) VALUES (382618661376229377, "Captain_Picard_Day.png");
+INSERT INTO badges (user_discord_id, badge_name) VALUES (875936903801737247, "Captain_Picard_Day.png");
+INSERT INTO badges (user_discord_id, badge_name) VALUES (873883287096033331, "Captain_Picard_Day.png");
