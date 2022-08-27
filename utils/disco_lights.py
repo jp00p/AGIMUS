21499decfaa5a5c7bfa1e50ee23c7da1e981454c
zmattingly <z@matting.ly>
2022-08-27 15:11:06 -0700

    add code to match log lines for colors on LightHandler, bump version

--- utils/disco_lights.py
+++ utils/disco_lights.py
@@ -2,6 +2,7 @@ import logging
 import random
 import re
 import requests
+from pprint import pprint
 from urllib3.exceptions import InsecureRequestWarning
 from huesdk import Hue # https://pypi.org/project/huesdk/
 
@@ -17,7 +18,8 @@ lights_config = config.get("lights")
 if lights_config:
   bridge_ip = lights_config.get("bridge_ip") # https://discovery.meethue.com/ get your IP
   bridge_user = lights_config.get("hue_username") # see below
-  ### 
+  lights_mode = lights_config.get("lights_mode")
+  ###
   # make a tiny file with this in it:
   # ----------------------------------
   # from huesdk import Hue
@@ -29,7 +31,7 @@ if lights_config:
   # you will get a username to use for your config file
   ###
 
-  
+
 
 class LightHandler(logging.Handler):
   def emit(self, record):
@@ -45,13 +47,15 @@ class LightHandler(logging.Handler):
       if not hue:
         return
 
+      print("hue present")
+
       target_light_name = lights_config.get("light_name")
-      
+
       if not target_light_name:
         return
-      
+
       log_line = record.getMessage()
-      
+
       if "*" not in log_line:
         return
 
@@ -60,74 +64,89 @@ class LightHandler(logging.Handler):
       if not light.is_on:
         return
 
-      light.set_color(hue=random.randint(1,65535), transition=3)
-           
-      # b.set_light(light_name, "xy", (random.random(), random.random()))
+      if lights_mode == "match_logs":
+        print(f"lights_mode: {lights_mode}")
+        hex = get_hex_from_logline(log_line)
+        light.set_color(hexa=hex, transition=3)
+        light.set_saturation(254)
+      else:
+        light.set_color(hue=random.randint(1,65535), transition=3)
 
-      # log_line = record.getMessage()
-      # split = split_ANSI(log_line)
-
-      # col = split.get("col")
-      # if not col:
-      #   return
-
-      # ansi_matches = re.findall(r"(\d+)m", col)
-      # ansi_number = 0
-      # for match in ansi_matches:
-      #   ansi_number += int(match)
-      # rgb = get_rgb_from_ansi(int(ansi_number))
-      # xy = get_xy_from_rgb(*rgb)
-      # bri = int(xy[1]*255)
-
-      # if not b.get_light(light_name, 'on'):
-      #   return
-
-      # command = {"transitiontime:": 0, "xy": xy, "bri": bri}
-      # b.set_light(light_name, command)
-
-
-# ANSI Methods
-def get_rgb_from_ansi(ansi_number):
-  index_R = ((ansi_number - 16) // 36)
-  rgb_R = 55 + index_R * 40 if index_R > 0 else 0
-  index_G = (((ansi_number - 16) % 36) // 6)
-  rgb_G = 55 + index_G * 40 if index_G > 0 else 0
-  index_B = ((ansi_number - 16) % 6)
-  rgb_B = 55 + index_B * 40 if index_B > 0 else 0
-
-  # Convert to floats
-  fR = rgb_R / 255
-  fG = rgb_G / 255
-  fB = rgb_B / 255
-
-  return [fR, fG, fB]
-
+#    _____    _______    _________.___
+#   /  _  \   \      \  /   _____/|   |
+#  /  /_\  \  /   |   \ \_____  \ |   |
+# /    |    \/    |    \/        \|   |
+# \____|__  /\____|__  /_______  /|___|
+#         \/         \/        \/
 split_ANSI_escape_sequences = re.compile(r"""
-    (?P<col>(\x1b     # literal ESC
-    \[       # literal [
-    [;\d]*   # zero or more digits or semicolons
-    [A-Za-z] # a letter
-    )*)
-    (?P<name>.*)
-    """, re.VERBOSE).match
+  (?P<col>(\x1b # literal ESC
+  \[            # literal [
+  [;\d]*        # zero or more digits or semicolons
+  [A-Za-z]      # a letter
+  )*)
+  (?P<name>.*)
+  """, re.VERBOSE).match
 
 def split_ANSI(s):
-    return split_ANSI_escape_sequences(s).groupdict()
+  return split_ANSI_escape_sequences(s).groupdict()
 
-# xy methods
-def get_xy_from_rgb(red, green, blue):
-    # gamma correction
-    red = pow((red + 0.055) / (1.0 + 0.055), 2.4) if red > 0.04045 else (red / 12.92)
-    green = pow((green + 0.055) / (1.0 + 0.055), 2.4) if green > 0.04045 else (green / 12.92)
-    blue =  pow((blue + 0.055) / (1.0 + 0.055), 2.4) if blue > 0.04045 else (blue / 12.92)
 
-    # convert rgb to xyz
-    x = red * 0.649926 + green * 0.103455 + blue * 0.197109
-    y = red * 0.234327 + green * 0.743075 + blue * 0.022598
-    z = green * 0.053077 + blue * 1.035763
+# _________        .__
+# \_   ___ \  ____ |  |   ___________
+# /    \  \/ /  _ \|  |  /  _ \_  __ \
+# \     \___(  <_> )  |_(  <_> )  | \/
+#  \______  /\____/|____/\____/|__|
+#         \/
+LOW_TO_RGB = {
+  "30": (46, 52, 55),    # Grey
+  "31": (205, 59, 40),   # Red
+  "32": (78, 154, 9),    # Green
+  "33": (196, 161, 36),  # Yellow
+  "34": (52, 101, 164),  # Blue
+  "35": (117, 80, 123),  # Magenta
+  "36": (64, 153, 155),  # Cyan
+  "37": (211, 215, 207), # Reset
+  "38": (211, 215, 207), # Reset
+  "39": (211, 215, 207), # Reset
+}
 
-    # convert xyz to xy
-    x = x / (x + y + z)
-    y = y / (x + y + z)
-     
-    return [x, y]
+HIGH_TO_RGB = {
+  "92": (208, 98, 101),  # Light Red
+  "93": (0, 245, 146),   # Light Green
+  "94": (225, 229, 0),   # Light Yellow
+  "95": (153, 149, 210), # Light Blue
+  "95": (237, 97, 230),  # Magenta
+  "96": (164, 73, 166),  # Light Magenta
+  "97": (0, 248, 255)    # Light Cyan
+}
+
+def get_hex_from_logline(msg):
+  split = split_ANSI(msg)
+  col = split.get("col")
+  if not col:
+    return "#dddddd"
+
+  ansi_matches = re.findall(r"(\d+)m", col)
+  ansi_number = 0
+  for match in ansi_matches:
+    ansi_number += int(match)
+    if ansi_number >= 30 and ansi_number <= 39:
+      break
+
+  hex = _get_hex_from_ansi(ansi_number)
+  return hex
+
+def _get_hex_from_ansi(ansi):
+  ansi = int(ansi)
+  if ansi >= 30 and ansi <= 39:
+    rgb = LOW_TO_RGB.get(str(ansi))
+  else:
+    rgb = HIGH_TO_RGB.get(str(ansi))
+
+  if rgb is None:
+    rgb = (211, 215, 207)
+
+  return _get_hex_from_rgb(rgb)
+
+def _get_hex_from_rgb(rgb):
+  return '%02x%02x%02x' % rgb
