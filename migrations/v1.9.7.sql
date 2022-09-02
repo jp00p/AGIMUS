c187989c9f6f82994eb76051ad0d28aa3f1109fd
zmattingly <z@matting.ly>
2022-09-01 18:00:12 -0700

    migration to correct affiliations for bajoran, vulcan, and mariposas badges

---
+++ migrations/v1.9.7.sql
@@ -0,0 +1,4 @@
+UPDATE badge_affiliation SET affiliation_name = "Las Mariposas" WHERE badge_filename = "Las_Mariposas_2020s.png" AND affiliation_name = "Las Maripsas";
+INSERT INTO badge_affiliation (badge_filename, affiliation_name) VALUES ("Bajor_Admission_Flag_A.png", "Bajor");
+INSERT INTO badge_affiliation (badge_filename, affiliation_name) VALUES ("Bajor_Admission_Flag_B.png", "Bajor");
+INSERT INTO badge_affiliation (badge_filename, affiliation_name) VALUES ("Vulcan_Science_Academy_(VSA).png", "Vulcan");
