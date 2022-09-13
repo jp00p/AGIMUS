8df2ac8831d6864e1095d1d37296b31406808d9e
Zane Mattingly <z@matting.ly>
2022-09-13 08:44:40 -0700

    Tweak: Remove Episode Title From Shimoda Nominator (#339)
    * remove episode title from shimoda nomination stuff
    
    * bump version

--- commands/shimoda.py
+++ commands/shimoda.py
@@ -4,10 +4,6 @@ class ShimodaModal(discord.ui.Modal):
   def __init__(self, *args, **kwargs) -> None:
     super().__init__(
       discord.ui.InputText(
-          label="Episode",
-          placeholder="Episode Title",
-      ),
-      discord.ui.InputText(
           label="Character",
           placeholder="Which Character?",
       ),
@@ -20,31 +16,36 @@ class ShimodaModal(discord.ui.Modal):
     )
 
   async def callback(self, interaction:discord.Interaction):
-      pollster_embed = discord.Embed(
-        title="New Shimoda Nomination",
-        description=f"{interaction.user.mention} has nominated a new Drunk Shimoda!",
-        fields=[
-          discord.EmbedField(name="Episode", value=self.children[0].value, inline=True),
-          discord.EmbedField(name="Character", value=self.children[1].value, inline=True),
-          discord.EmbedField(name="Reason", value=self.children[2].value, inline=False)
-        ],
-        color=discord.Color.random()
-      )
-      roles = await bot.current_guild.fetch_roles()
-      pollster_role = None
-      for r in roles:
-        if r.name == config['roles']['shimoda_pollsters']:
-          pollster_role = r
+    member = await bot.current_guild.fetch_member(interaction.user.id)
+    pollster_embed = discord.Embed(
+      title="New Shimoda Nomination",
+      description=f"{member.mention} has nominated a new Drunk Shimoda!",
+      fields=[
+        discord.EmbedField(name="Character", value=self.children[0].value, inline=False),
+        discord.EmbedField(name="Reason", value=self.children[1].value, inline=False)
+      ],
+      color=discord.Color.random()
+    )
+    roles = await bot.current_guild.fetch_roles()
+    pollster_role = None
+    for r in roles:
+      if r.name == config['roles']['shimoda_pollsters']:
+        pollster_role = r
 
-      recipients = pollster_role.members
-      for recipient in recipients:
-        await recipient.send(embed=pollster_embed)
+    recipients = pollster_role.members
+    for recipient in recipients:
+      await recipient.send(embed=pollster_embed)
 
-      await interaction.response.send_message(embed=discord.Embed(
-        title="Drunk Shimoda Nomination Successfully Sent!",
-        color=discord.Color.green()
-      ), ephemeral=True)
+    nominator_embed = discord.Embed(
+      title="Drunk Shimoda Nomination Successfully Sent!",
+      color=discord.Color.green(),
+      fields=[
+        discord.EmbedField(name="Character", value=self.children[0].value, inline=False),
+        discord.EmbedField(name="Reason", value=self.children[1].value, inline=False)
+      ]
+    )
 
+    await interaction.response.send_message(embed=nominator_embed, ephemeral=True)
 
 
 @bot.slash_command(
