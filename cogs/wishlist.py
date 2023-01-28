58d946e6042a77338dbec221194580b4047bcbf7
zm <z@matting.ly>
2023-01-27 20:06:06 -0800

    more explicit filtering of wishlist self-matches

--- cogs/wishlist.py
+++ cogs/wishlist.py
@@ -200,30 +200,36 @@ class Wishlist(commands.Cog):
   @commands.check(access_check)
   async def matches(self, ctx:discord.ApplicationContext):
     await ctx.defer(ephemeral=True)
-    user_discord_id = ctx.author.id
+    author_discord_id = ctx.author.id
 
     logger.info(f"{ctx.author.display_name} is checking for {Style.BRIGHT}matches{Style.RESET_ALL} to their {Style.BRIGHT}wishlist{Style.RESET_ALL}")
 
     # Get all the users and the badgenames that have the badges the user wants
-    wishlist_matches = db_get_wishlist_matches(user_discord_id)
+    wishlist_matches = db_get_wishlist_matches(author_discord_id)
     wishlist_aggregate = {}
     if wishlist_matches:
       for match in wishlist_matches:
         user_id = match['user_discord_id']
+        if user_id == author_discord_id:
+          continue
+
         user_record = wishlist_aggregate.get(user_id)
-        if not user_record and user_id != user_discord_id:
+        if not user_record:
           wishlist_aggregate[user_id] = [match]
         else:
           wishlist_aggregate[user_id].append(match)
 
     # Get all the users and the badgenames that want badges that the user has
-    inventory_matches = db_get_wishlist_inventory_matches(user_discord_id)
+    inventory_matches = db_get_wishlist_inventory_matches(author_discord_id)
     inventory_aggregate = {}
     if inventory_matches:
       for match in inventory_matches:
         user_id = match['user_discord_id']
+        if user_id == author_discord_id:
+          continue
+
         user_record = inventory_aggregate.get(user_id)
-        if not user_record:
+        if not user_record and user_id != author_discord_id:
           inventory_aggregate[user_id] = [match]
         else:
           inventory_aggregate[user_id].append(match)
