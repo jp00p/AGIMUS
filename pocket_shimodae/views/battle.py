ce2dd646289dc7638e5447ad3e99003404e21e10
Jpop Wilson <asimovsghost@gmail.com>
2023-03-07 10:29:28 -0800

    wake up

--- pocket_shimodae/views/battle.py
+++ pocket_shimodae/views/battle.py
@@ -1,10 +1,6 @@
 from typing import List
-
 from common import *
-
-from ..objects import (BattleStates, BattleTypes, Poshimo, PoshimoBattle,
-                       PoshimoMove, PoshimoTrainer, STAT_NAMES)
-
+from ..objects import BattleStates, BattleTypes, Poshimo, PoshimoBattle, PoshimoMove, PoshimoTrainer, STAT_NAMES
 from ..ui import *
 from . import main_menu as mm
 
