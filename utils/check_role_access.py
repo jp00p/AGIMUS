d2b58528a81cb498406369777157dfe6aeafa2a5
Zane Mattingly <z@matting.ly>
2023-04-27 11:52:06 -0700

    new message_commands to allow pin/unpin for Lt Commanders (#420)

---
+++ utils/check_role_access.py
@@ -0,0 +1,24 @@
+from common import *
+
+# @commands.check decorator function
+# Can be injected in between @commands.check and your slash function to
+# restrict access to the "allowed_roles" from the command config by matching it with the function name
+async def role_check(ctx):
+  ctx_type = type(ctx).__name__
+  try:
+    command_config = config["commands"][f"{ctx.command}"]
+    allowed_roles = command_config.get('allowed_roles')
+    if not allowed_roles:
+      return True
+
+    allowed_role_ids = get_role_ids_list(allowed_roles)
+    user_role_ids = [r.id for r in ctx.author.roles]
+
+    allowed = any(user_rid in allowed_role_ids for user_rid in user_role_ids)
+    if not allowed:
+      if ctx_type == 'ApplicationContext':
+        await ctx.respond(f"{get_emoji('guinan_beanflick_stance_threat')} Sorry! You don't have the proper security clearance to use this command.", ephemeral=True)
+    else:
+      return True
+  except Exception as e:
+    logger.info(e)
