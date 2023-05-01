c0c6c943d8200c020f509922e2c9bae1bdff48a4
Zane Mattingly <z@matting.ly>
2023-05-01 09:55:51 -0700

    Migration: Correct "Research/Science" Badge Type (#421)
    * migration file to correct "Research/Science" badge type
    
    * bonus: associate Mikulak badge with Mikulak affiliation

---
+++ migrations/v1.25.0.sql
@@ -0,0 +1,2 @@
+UPDATE badge_type SET type_name = "Science/Research" WHERE type_name = "Research/Science";
+INSERT IGNORE INTO badge_affiliation (badge_filename, affiliation_name) VALUES ("Mikulak.png", "Mikulak");
