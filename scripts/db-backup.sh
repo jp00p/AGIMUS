6150b6e838442da9b87adbe08a3d7f5207a3d0eb
Chris W Jones <chris@christopherjones.us>
2023-06-08 15:12:36 -0400

    Fix db backup command

--- scripts/db-backup.sh
+++ scripts/db-backup.sh
@@ -24,4 +24,4 @@ function log() {
 }
 
 log "Uploading ${DB_NAME} to s3://${S3_BUCKET_NAME}/${DB_DUMP_S3_PREFIX}/${DB_DUMP_FILENAME_WITH_TIMESTAMP}.gz"
-bat Makefile | gzip -9 | s3cmd --host nyc3.digitaloceanspaces.com --region nyc3 put - "s3://${S3_BUCKET_NAME}/${DB_DUMP_S3_PREFIX}/${DB_DUMP_FILENAME_WITH_TIMESTAMP}.gz"
+mysqldump -h"${DB_HOST}" -u"${DB_USER}" -p"${DB_PASS}" -B "${DB_NAME}" | gzip -9 | s3cmd --host nyc3.digitaloceanspaces.com --region nyc3 put - "s3://${S3_BUCKET_NAME}/${DB_DUMP_S3_PREFIX}/${DB_DUMP_FILENAME_WITH_TIMESTAMP}.gz"
