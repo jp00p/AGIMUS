89cd59c873036b490ff2d5f9ec2768d989a51022
Chris W Jones <chris@christopherjones.us>
2023-06-08 15:04:42 -0400

    Autowire s3 key and secret with proper env var names

--- scripts/db-backup.sh
+++ scripts/db-backup.sh
@@ -24,4 +24,4 @@ function log() {
 }
 
 log "Uploading ${DB_NAME} to s3://${S3_BUCKET_NAME}/${DB_DUMP_S3_PREFIX}/${DB_DUMP_FILENAME_WITH_TIMESTAMP}.gz"
-bat Makefile | gzip -9 | s3cmd --secret_key "${S3_SECRET_KEY}" --access_key "${S3_ACCESS_KEY}" put - "s3://${S3_BUCKET_NAME}/${DB_DUMP_S3_PREFIX}/${DB_DUMP_FILENAME_WITH_TIMESTAMP}.gz"
+bat Makefile | gzip -9 | s3cmd put - "s3://${S3_BUCKET_NAME}/${DB_DUMP_S3_PREFIX}/${DB_DUMP_FILENAME_WITH_TIMESTAMP}.gz"
