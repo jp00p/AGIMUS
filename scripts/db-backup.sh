2ab5bb84b74653106e1fe0f35e48f6b12b342ca7
Chris W Jones <chris@christopherjones.us>
2023-06-08 14:10:30 -0400

    Update db-backup and db-restore targets in Makefile
    This totally reworks how and where database backups are stored.  Instead
    of storing database dumps in a git repo, store them in a DigitalOcean
    bucket.

---
+++ scripts/db-backup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+function setupDotEnv() {
+    if [ -f .env ]
+    then
+        set -o allexport; source .env; set +o allexport
+    fi
+}
+setupDotEnv
+
+# To get log messages, call this script with DEBUG=true
+function log() {
+  local message="${1}"
+  local debug_set="${DEBUG:-false}"
+
+  if [ "${debug_set}" = "false" ]; then
+    return
+  fi
+
+  printf "%s: %s\n" "$(date --iso-8601=seconds)" "${message}" >&2
+}
+
+log "Uploading ${DB_NAME} to s3://${S3_BUCKET_NAME}/${DB_DUMP_S3_PREFIX}/${DB_DUMP_FILENAME_WITH_TIMESTAMP}.gz"
+bat Makefile | gzip -9 | s3cmd --secret_key "${S3_SECRET_KEY}" --access_key "${S3_ACCESS_KEY}" put - "s3://${S3_BUCKET_NAME}/${DB_DUMP_S3_PREFIX}/${DB_DUMP_FILENAME_WITH_TIMESTAMP}.gz"
---
+++ scripts/db-restore.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+function setupDotEnv() {
+    if [ -f .env ]
+    then
+        set -o allexport; source .env; set +o allexport
+    fi
+}
+setupDotEnv
+
+# To get log messages, call this script with DEBUG=true
+function log() {
+  local message="${1}"
+  local debug_set="${DEBUG:-false}"
+
+  if [ "${debug_set}" = "false" ]; then
+    return
+  fi
+
+  printf "%s: %s\n" "$(date --iso-8601=seconds)" "${message}" >&2
+}
+
+BACKUP_TO_RESTORE="${1:-}"
+
+if [ "${BACKUP_TO_RESTORE}" = "--help"  ] || [ "${BACKUP_TO_RESTORE}" = "-h" ]
+then
+    echo "Usage: ${0} <backup_to_restore>.sql.gz"
+    echo "Can be found with 's3cmd ls s3://${S3_BUCKET_NAME}/${DB_DUMP_S3_PREFIX}/'"
+    exit 1
+fi
+
+if [ -z "${BACKUP_TO_RESTORE}" ]
+then
+    # List the bucket contents, filter out the directories, sort by time with newest first, take the first one, return only the s3 path
+    BACKUP_TO_RESTORE=$(s3cmd --secret_key "${S3_SECRET_KEY}" --access_key "${S3_ACCESS_KEY}" ls "s3://${S3_BUCKET_NAME}/${DB_DUMP_S3_PREFIX}/" | grep -v DIR | sort -rnk2 | head -1 | awk '{ print $4 }')
+    log "No backup specified, using one of today's backups: ${BACKUP_TO_RESTORE}"
+fi
+
+log "Downloading ${BACKUP_TO_RESTORE} to ${DB_DUMP_FILENAME_WITH_TIMESTAMP}"
+s3cmd --secret_key "${S3_SECRET_KEY}" --access_key "${S3_ACCESS_KEY}" --no-progress get "${BACKUP_TO_RESTORE}" - | gunzip > "${DB_DUMP_FILENAME_WITH_TIMESTAMP}"
+
+log "Dropping database ${DB_NAME}"
+mysql -h"${DB_HOST}" -u"${DB_USER}" -p"${DB_PASS}" <<< "DROP DATABASE IF EXISTS FoD;"
+log "Creating database ${DB_NAME}"
+mysql -h"${DB_HOST}" -u"${DB_USER}" -p"${DB_PASS}" <<< "create database FoD;"
+log "Restoring database ${DB_NAME} from ${DB_DUMP_FILENAME_WITH_TIMESTAMP}"
+mysql -h"${DB_HOST}" -u"${DB_USER}" -p"${DB_PASS}" "${DB_NAME}" < "${DB_DUMP_FILENAME_WITH_TIMESTAMP}"
