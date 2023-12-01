3ae3812cc22e62a73e00d203153944e1639659ed
Bryan Levay <bryan@bryanlevay.com>
2023-12-01 12:44:29 -0800

    remove message from unpin since it doesn't really have context

--- commands/pin.py
+++ commands/pin.py
@@ -1,18 +1,26 @@
 from common import *
 from utils.check_role_access import role_check
 
+f = open("./data/pin_messages.json")
+random_pin_messages = json.load(f)
+f.close()
+
+
 @bot.message_command(
   name="pin",
   description="Pin (Admin Only)"
 )
 @commands.check(role_check)
 async def pin(ctx, message: discord.Message):
-  logger.info(f"{ctx.author.display_name} is pinning message {message.id} in {ctx.channel.name}")
+  pinning_user = ctx.author.display_name
+  logger.info(
+    f"{pinning_user} is pinning message {message.id} in {ctx.channel.name}")
   if message.pinned:
     await ctx.respond(f"Whoa there, this message is already pinned! No action taken.", ephemeral=True)
   else:
     await message.pin()
     await ctx.respond(f"Message pinned! 📌", ephemeral=True)
+    await ctx.channel.send(random.choice(random_pin_messages["messages"]).format(user=pinning_user))
 
 
 @bot.message_command(
@@ -21,7 +29,9 @@ async def pin(ctx, message: discord.Message):
 )
 @commands.check(role_check)
 async def unpin(ctx, message: discord.Message):
-  logger.info(f"{ctx.author.display_name} is unpinning message {message.id} in {ctx.channel.name}")
+  pinning_user = ctx.author.display_name
+  logger.info(
+    f"{ctx.author.display_name} is unpinning message {message.id} in {ctx.channel.name}")
   if not message.pinned:
     await ctx.respond(f"Whoa there, can't unpin this message because it is not already pinned! No action taken.", ephemeral=True)
   else:
