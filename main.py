c4727d83c8499f6e66d4a71a5e1e3d61ce32eb0c
Zane Mattingly <z@matting.ly>
2024-01-31 19:15:20 -0800

    generate bar chart for daily activity, rename group to myinfo

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ from commands.speak import speak, speak_embed
 from commands.trekduel import trekduel
 from commands.trektalk import trektalk
 from commands.tuvix import tuvix
-from commands.userinfo import top_channels
+from commands.myinfo import top_channels
 
 # Slash Command Groups
 import commands.birthday
