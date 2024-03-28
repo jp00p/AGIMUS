ceda87e0bb18f4e234c454659eb3c733ccb0ec9a
Zane Mattingly <z@matting.ly>
2024-03-28 07:36:20 -0700

    log occurances in captains-log

--- handlers/reply_restricted.py
+++ handlers/reply_restricted.py
@@ -8,13 +8,21 @@ async def handle_reply_restricted(message:discord.Message):
   if message.reference or message.mentions:
     try:
       notify_embed = discord.Embed(
-        title=f"Whoops, replies restricted.",
+        title=f"Whoops, Replies Restricted!",
         description=f"Sorry, your message has been removed. We don't allow replies/feedback in {message.channel.mention}, it's just intended as space to simply post messages without feedback.\n\nPlease see the channel description! Thanks.",
         color=discord.Color.red()
       )
-      notify_embed.set_footer(text="S'all good, no worries. This is an automated message, if you have questions please ask a moderator!")
+      notify_embed.set_footer(text="This is an automated message, if you have questions please ask a moderator!")
       await message.author.send(embed=notify_embed)
     except discord.Forbidden as e:
-      logger.info(f"Unable to send Cornmander stripping message to {message.author.display_name}, they have their DMs closed.")
+      logger.info(f"Unable to send Replies Restricted message to {message.author.display_name}, they have their DMs closed.")
       pass
     await message.delete()
+
+    server_logs_channel = bot.get_channel(get_channel_id(config["server_logs_channel"]))
+    await server_logs_channel.send(embed=discord.Embed(
+      title=f"{message.author.display_name} Posted A Restricted Reply",
+      description=f"{message.author.mention} posted a reply to someone in {message.channel.mention}.\n\n"
+                  "It was automatically removed and they were sent a DM to explain why.",
+      color=discord.Color.red()
+    ))
