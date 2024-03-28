3d1a622aec1cd3df53a13afd3aad8cb76958de36
Zane Mattingly <z@matting.ly>
2024-03-28 07:47:44 -0700

    Restrict Replies/Mentions In #plasma-vent And #neelixs-morale-office (#510)
    * restrict replies in plasma-vent and neelixs-morale, correct cornmander
    date check, add MAP drop
    
    * log occurances in captains-log
    
    * bump version

--- main.py
+++ main.py
@@ -93,6 +93,7 @@ from handlers.alerts import handle_alerts
 from handlers.bot_autoresponse import handle_bot_affirmations
 from handlers.cornmander import handle_cornmander
 from handlers.loudbot import handle_loudbot
+from handlers.reply_restricted import handle_reply_restricted
 from handlers.save_message import save_message_to_db
 from handlers.server_logs import *
 from handlers.starboard import get_all_starboard_posts, handle_starboard_reactions
@@ -148,6 +149,7 @@ async def on_message(message:discord.Message):
     await handle_loudbot(message)
     await handle_alerts(message)
     await handle_cornmander(message)
+    await handle_reply_restricted(message)
   except Exception as e:
     logger.error(f"{Fore.RED}<! ERROR: Encountered error in handlers !> {e}{Fore.RESET}")
     logger.error(traceback.format_exc())
