a4592c139a2ca2e4ca35bfe59ca6de768a818575
zm <z@matting.ly>
2024-05-23 07:26:10 -0700

    just use channel.purge() rather than back lookup

--- tasks/channel_cleanup.py
+++ tasks/channel_cleanup.py
@@ -8,29 +8,21 @@ def channel_cleanup_task(bot):
     if not enabled:
       return
 
-    pst_tz = pytz.timezone('America/Los_Angeles')
-    raw_now = datetime.utcnow().replace(tzinfo=pytz.utc)
-    aware_now = pst_tz.normalize(raw_now.astimezone(pst_tz))
-
-    one_day_ago = aware_now - timedelta(days=1)
-
     channel_ids = get_channel_ids_list(config["tasks"]["channel_cleanup"]["channels"])
     for channel_id in channel_ids:
       channel = bot.get_channel(channel_id)
+      if not channel:
+        continue
+
+      # Just nuke the entire channel history
+      await channel.purge()
 
       self_destruct_embed = discord.Embed(
-        title="Deleting previous messages...",
+        title="Channel Nuked From Orbit! 💥",
         color=discord.Color.dark_red()
       )
       self_destruct_embed.set_footer(text="This message will self-destruct in 10 minutes... 💣")
       self_destruct_message = await channel.send(embed=self_destruct_embed)
-
-      async for message in channel.history(before=one_day_ago):
-        try:
-          await message.delete()
-        except Exception as e:
-          logger.error(f"Error channel_cleanup_task: {e}")
-          logger.info(traceback.format_exc())
 
       await asyncio.sleep(600)
       await self_destruct_message.delete()
