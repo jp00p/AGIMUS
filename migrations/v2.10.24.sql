c4862c1bffd5e9f016e1f96481b1a74397007763
Peter of the Norse <rahmcoff@gmail.org>
2024-09-20 18:49:40 -0600

    And the rest of the Talaxian badges

--- migrations/v2.10.24.sql
+++ migrations/v2.10.24.sql
@@ -1,1 +1,4 @@
 UPDATE badge_info SET badge_url = "https://www.startrekdesignproject.com/symbols/talaxian-a1" WHERE badge_filename = "Talaxian_A1.png";
+UPDATE badge_info SET badge_url = "https://www.startrekdesignproject.com/symbols/talaxian-a2" WHERE badge_filename = "Talaxian_A2.png";
+UPDATE badge_info SET badge_url = "https://www.startrekdesignproject.com/symbols/talaxian-a3" WHERE badge_filename = "Talaxian_A3.png";
+UPDATE badge_info SET badge_url = "https://www.startrekdesignproject.com/symbols/talaxian-b" WHERE badge_filename = "Talaxian_B.png";
