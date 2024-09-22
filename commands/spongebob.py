f99691698ed3d979cf146e99088695fcb4df1346
zm <z@matting.ly>
2024-09-22 15:59:12 -0700

    use author instead of full image

--- commands/spongebob.py
+++ commands/spongebob.py
@@ -18,7 +18,9 @@ async def spongebob(ctx:discord.ApplicationContext, content:str):
       title="SpongeBobbed!",
       description=sponged,
       color=discord.Color.blurple()
-    ).set_image(url="https://i.imgur.com/eOaiaZ6.png")
+    ).set_author(
+      url="https://i.imgur.com/bjDaNDJ.png"
+    )
   )
 
 def sponge(bob):
