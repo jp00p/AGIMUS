3a69a742510bedbe8193ed83d07200d6b421f596
Peter of the Norse <rahmcoff@users.noreply.github.com>
2025-03-29 12:03:06 -0600

    Updated `/fmk`, `/trekduel`, `/tuvix`, and `/dustbuster` to include l… (#587)
    * Updated `/fmk`, `/trekduel`, `/tuvix`, and `/dustbuster` to include links to Memory Alpha
    
    Also, updated the list of characters a bit
    
    * Add list of link exceptions
    
    ---------
    
    Co-authored-by: Peter of the Norse <rahmcoff@gmail.org>

--- common.py
+++ common.py
@@ -422,3 +422,27 @@ def remove_emoji(s: str) -> str:
   Cleans out values unprintable in our font.  cp1252 because there are a handful of chars in the font not in latin_1
   """
   return s.replace("€", '').encode("cp1252", errors="ignore").decode("cp1252").strip()
+
+
+IRREGULAR_MEM_ALPHA_LINKS = None
+
+def make_memory_alpha_link(name: str) -> str:
+  """
+  Convert the name of this person/ship/tech into a link to https://memory-alpha.fandom.com.  There are exceptions, and
+  they are stored in characters_mem_alpha_links.json.  But it’s still better
+  """
+  global IRREGULAR_MEM_ALPHA_LINKS
+  if IRREGULAR_MEM_ALPHA_LINKS is None:
+    with open("data/characters_mem_alpha_links.json") as f:
+      IRREGULAR_MEM_ALPHA_LINKS = json.load(f)
+  
+  if name.startswith("A "):
+    return name
+  if name in IRREGULAR_MEM_ALPHA_LINKS:
+    link = IRREGULAR_MEM_ALPHA_LINKS[name]
+    if link:  # can be null if no link exists
+      return f"[{name}]({link})"
+    else:
+      return name
+  link_name = re.sub(r'".*" ', '', name) if '"' in name else name  # "nicknames" are not included in the links
+  return f"[{name}](https://memory-alpha.fandom.com/wiki/{link_name.replace(' ', '_')})"
