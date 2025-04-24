d50b480e517d9ce02bd118f3f8aecc8ad8370c2f
zm <z@matting.ly>
2025-04-24 06:33:55 -0700

    `/crystals vault` wip

--- cogs/crystals.py
+++ cogs/crystals.py
@@ -9,53 +9,65 @@ from utils.image_utils import *
 from utils.check_channel_access import access_check
 
 
-HARMONIZE_ENGAGE_FLAVORS = [
-  'Discharging the Enerjons',
-  'Charging up the EPS conduits',
-  'Transfering all power from the Life Support Systems',
-  'Ejecting the Warp Core',
-  'Placing something dangerous near the Warp Core',
-  'Diverting power from the Holodeck Safety Protocol Enforcer',
-  'Flooding the Jefferies Tubes with Omicron Radiation',
-  'Erecting a Level 10 Force Field'
-]
-
-RARITY_SUCCESS_MESSAGES = {
-  'common': [
-    "Another day, another Crystal for {user}.",
-    "One more for {user}.",
-    "That's a decent one {user}.",
-    "A decent *crystal* for {user}, but an incredible *member* of The Hood!",
-    "A fresh steaming Crystal for {user}!",
-    "I'd rate that a 3.6... Not great, not terrible {user}."
-  ],
-  'uncommon': [
-    "Heyyy! Lookin pretty good {user}.",
-    "Oo, not bad {user}!",
-    "Sweet {user}! Purty.",
-  ],
-  'rare': [
-    "SHINY! Congrats {user}! Hold onto that one!",
-    "SPARKLY! {user}'s in rare Form!",
-    "SCINTILATING! Spectacular too, very nice {user}!"
-  ],
-  'legendary': [
-    "Whoa!!! Legen-dairy! Is this some kind of milk-based crystal {user}!?",
-    "Well GOTDAYUM!!! That's some shiny shiny shiny {user}!",
-    "FIYAH!!! Crystalline Goodness for {user}!"
-  ],
-  'mythic': [
-    "HOLY **FUCKING** SHIT!!!!! {user} got a ***MYTHIC*** Crystal!?! INCREDIBLE! " + get_emoji('drunk_shimoda_smile_happy'),
-    "SWEET JESUS!!!!! Are you kiddin me {user}, *MYTHIC*!? " + get_emoji('zephram_sweet_jesus_wow_whoa'),
-    "OH DEAR LORD!!!!!! One freshly minted **MYTHIC** Crystal for {user}!? " + get_emoji('barclay_omg_wow_shock')
-  ]
-}
-
+# _________
+# \_   ___ \  ____   ____
+# /    \  \/ /  _ \ / ___\
+# \     \___(  <_> ) /_/  >
+#  \______  /\____/\___  /
+#         \/      /_____/
 class Crystals(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
+    self.HARMONIZE_ENGAGE_FLAVORS = [
+      'Discharging the Enerjons',
+      'Charging up the EPS conduits',
+      'Transfering all power from the Life Support Systems',
+      'Ejecting the Warp Core',
+      'Placing something dangerous near the Warp Core',
+      'Diverting power from the Holodeck Safety Protocol Enforcer',
+      'Flooding the Jefferies Tubes with Omicron Radiation',
+      'Erecting a Level 10 Force Field'
+    ]
 
-  crystals_group = discord.SlashCommandGroup("crystals", "Crystals Management.")
+  # .____    .__          __
+  # |    |   |__| _______/  |_  ____   ____   ___________  ______
+  # |    |   |  |/  ___/\   __\/ __ \ /    \_/ __ \_  __ \/  ___/
+  # |    |___|  |\___ \  |  | \  ___/|   |  \  ___/|  | \/\___ \
+  # |_______ \__/____  > |__|  \___  >___|  /\___  >__|  /____  >
+  #         \/       \/            \/     \/     \/           \/
+  @commands.Cog.listener()
+  async def on_ready(self):
+    # Some of these load emoji so need to wait for on_ready() for make sure they're in the config
+    self.RARITY_SUCCESS_MESSAGES = {
+      'common': [
+        "Another day, another Crystal for {user}.",
+        "One more for {user}.",
+        "That's a decent one {user}.",
+        "A decent *crystal* for {user}, but an incredible *member* of The Hood!",
+        "A fresh steaming Crystal for {user}!",
+        "I'd rate that a 3.6... Not great, not terrible {user}."
+      ],
+      'uncommon': [
+        "Heyyy! Lookin pretty good {user}.",
+        "Oo, not bad {user}!",
+        "Sweet {user}! Purty.",
+      ],
+      'rare': [
+        "SHINY! Congrats {user}! Hold onto that one!",
+        "SPARKLY! {user}'s in rare Form!",
+        "SCINTILATING! Spectacular too, very nice {user}!"
+      ],
+      'legendary': [
+        "Whoa!!! Legen-dairy! Is this some kind of milk-based crystal {user}!?",
+        "Well GOTDAYUM!!! That's some shiny shiny shiny {user}!",
+        "FIYAH!!! Crystalline Goodness for {user}!"
+      ],
+      'mythic': [
+        "HOLY **FUCKING** SHIT!!!!! {user} got a ***MYTHIC*** Crystal!?! INCREDIBLE! " + get_emoji('drunk_shimoda_smile_happy'),
+        "SWEET JESUS!!!!! Are you kiddin me {user}, *MYTHIC*!? " + get_emoji('zephram_sweet_jesus_wow_whoa'),
+        "OH DEAR LORD!!!!!! One freshly minted **MYTHIC** Crystal for {user}!? " + get_emoji('barclay_omg_wow_shock')
+      ]
+    }
 
   #    _____          __                                     .__          __
   #   /  _  \  __ ___/  |_  ____   ____  ____   _____ ______ |  |   _____/  |_  ____   ______
@@ -160,6 +172,14 @@ class Crystals(commands.Cog):
     return options
 
 
+  # _________                                           .___
+  # \_   ___ \  ____   _____   _____ _____    ____    __| _/______
+  # /    \  \/ /  _ \ /     \ /     \\__  \  /    \  / __ |/  ___/
+  # \     \___(  <_> )  Y Y  \  Y Y  \/ __ \|   |  \/ /_/ |\___ \
+  #  \______  /\____/|__|_|  /__|_|  (____  /___|  /\____ /____  >
+  #         \/             \/      \/     \/     \/      \/    \/
+  crystals_group = discord.SlashCommandGroup("crystals", "Crystals Management.")
+
   # __________              .__  .__               __
   # \______   \ ____ ______ |  | |__| ____ _____ _/  |_  ____
   #  |       _// __ \\____ \|  | |  |/ ___\\__  \\   __\/ __ \
@@ -170,7 +190,7 @@ class Crystals(commands.Cog):
   @commands.check(access_check)
   async def replicate(self, ctx: discord.ApplicationContext):
     await ctx.defer(ephemeral=True)
-    cog_bot = self.bot
+    cog = self
     user = ctx.user
     user_id = user.id
 
@@ -225,7 +245,7 @@ class Crystals(commands.Cog):
 
         confirmation_embed = discord.Embed(
           title='Crystal Replication In Progress!',
-          description=f"{random.choice(HARMONIZE_ENGAGE_FLAVORS)}, results incoming momentarily!",
+          description=f"{random.choice(cog.HARMONIZE_ENGAGE_FLAVORS)}, results incoming momentarily!",
           color=discord.Color.teal()
         )
         lcars_slider_gifs = [
@@ -238,7 +258,7 @@ class Crystals(commands.Cog):
 
         discord_file, effect_filename = await generate_crystal_replicator_confirmation_frames(crystal)
 
-        success_message = random.choice(RARITY_SUCCESS_MESSAGES[crystal['rarity_name'].lower()]).format(user=user.mention)
+        success_message = random.choice(cog.RARITY_SUCCESS_MESSAGES[crystal['rarity_name'].lower()]).format(user=user.mention)
         channel_embed = discord.Embed(
           title='CRYSTAL MATERIALIZATION COMPLETE',
           description=f"A fresh Crystal Buffer Pattern shunts into the replicator, the familiar hum fills the air, and the result is...\n### **{crystal['crystal_name']}**!\n\n{success_message}",
@@ -249,7 +269,7 @@ class Crystals(commands.Cog):
         channel_embed.set_image(url=f"attachment://{effect_filename}")
         channel_embed.set_footer(text="Use `/crystals attune` to attach it to one of your Badges!")
 
-        gelrak_v = await cog_bot.fetch_channel(get_channel_id("gelrak-v"))
+        gelrak_v = await cog.bot.fetch_channel(get_channel_id("gelrak-v"))
         await gelrak_v.send(embed=channel_embed, file=discord_file)
 
       @discord.ui.button(label="Cancel", style=discord.ButtonStyle.gray)
@@ -269,6 +289,46 @@ class Crystals(commands.Cog):
     view = ConfirmCancelView()
     await ctx.respond(embed=replicator_embed, view=view, ephemeral=True)
     view.message = await ctx.interaction.original_response()
+
+
+  # ____   ____            .__   __
+  # \   \ /   /____   __ __|  |_/  |_
+  #  \   Y   /\__  \ |  |  \  |\   __\
+  #   \     /  / __ \|  |  /  |_|  |
+  #    \___/  (____  /____/|____/__|
+  #                \/
+  @crystals_group.slash_command(name="vault", description="Access your unattuned Crystal vault.")
+  async def crystals_vault(self, ctx: discord.ApplicationContext):
+    user_id = ctx.user.id
+    crystals = await db_get_user_unattuned_crystals(user_id)
+
+    if not crystals:
+      embed = discord.Embed(
+        title="Crystal Vault",
+        description="You currently have no unattuned crystals in your vault.",
+        color=discord.Color.dark_purple()
+      )
+      await ctx.respond(embed=embed, ephemeral=True)
+      return
+
+    # Group crystals by rarity
+    crystals_by_rarity = {}
+    for crystal in crystals:
+      rarity = crystal['rarity_rank']
+      if rarity not in crystals_by_rarity:
+        crystals_by_rarity[rarity] = []
+      crystals_by_rarity[rarity].append(crystal)
+
+    # Build the initial landing embed
+    embed = discord.Embed(
+      title="Crystal Vault",
+      description="Welcome to your Crystal Vault. Select a rarity below to view your unattuned Crystals.",
+      color=discord.Color.blurple()
+    )
+    embed.set_image(url="https://example.com/vault_landing_image.png")  # Replace with actual asset
+
+    view = CrystalsVaultView(user_id, crystals_by_rarity)
+    await ctx.respond(embed=embed, view=view, ephemeral=True)
 
 
   #    _____   __    __
@@ -544,6 +604,121 @@ class Crystals(commands.Cog):
     view.message = await ctx.interaction.original_response()
 
 
+
+# ____   ____            .__   __    ____   ____.__
+# \   \ /   /____   __ __|  |_/  |_  \   \ /   /|__| ______  _  __
+#  \   Y   /\__  \ |  |  \  |\   __\  \   Y   / |  |/ __ \ \/ \/ /
+#   \     /  / __ \|  |  /  |_|  |     \     /  |  \  ___/\     /
+#    \___/  (____  /____/|____/__|      \___/   |__|\___  >\/\_/
+#                \/                                     \/
+class CrystalsVaultView(discord.ui.View):
+  def __init__(self, user_id: int, crystals_by_rarity: dict):
+    super().__init__(timeout=180)
+    self.user_id = user_id
+    self.crystals_by_rarity = crystals_by_rarity
+    self.add_item(RaritySelect(self))
+
+class RaritySelect(discord.ui.Select):
+  def __init__(self, parent_view: CrystalsVaultView):
+    self.parent_view = parent_view
+    options = [
+      discord.SelectOption(
+        label=f"{crystals[0]['rarity_name']} ({len(crystals)})",
+        emoji=crystals[0]['emoji'],
+        value=rarity
+      )
+      for rarity, crystals in parent_view.crystals_by_rarity.items()
+    ]
+    super().__init__(placeholder="Choose Rarity", options=options)
+
+  async def callback(self, interaction: discord.Interaction):
+    rarity = self.values[0]
+    rarity_level_crystals = self.parent_view.crystals_by_rarity[rarity]
+    pages, embeds = await generate_paginated_crystal_rarity_pages(self.parent_view.user_id, rarity_level_crystals)
+
+    paginator = CrystalsVaultPaginator(
+      user_id=self.parent_view.user_id,
+      rarity=rarity,
+      pages=pages,
+      embeds=embeds
+    )
+    paginator.add_item(RaritySelect(self.parent_view))
+    await interaction.response.edit_message(
+      embed=embeds[0],
+      attachments=[pages[0]],
+      view=paginator
+    )
+
+class CrystalsVaultPaginator(discord.ui.View):
+  def __init__(self, user_id: int, rarity: str, pages: list[discord.File], embeds: list[discord.Embed]):
+    super().__init__(timeout=180)
+    self.user_id = user_id
+    self.rarity = rarity
+    self.pages = pages
+    self.embeds = embeds
+    self.current_page = 0
+    self.total_pages = len(pages)
+
+    self.prev_button = discord.ui.Button(label="◀ Prev", style=discord.ButtonStyle.secondary, disabled=self.total_pages <= 1)
+    self.prev_button.callback = self.prev_page
+    self.add_item(self.prev_button)
+
+    self.page_indicator = discord.ui.Button(label=f"Page {self.current_page + 1}/{self.total_pages}", disabled=True)
+    self.add_item(self.page_indicator)
+
+    self.next_button = discord.ui.Button(label="Next ▶", style=discord.ButtonStyle.secondary, disabled=self.total_pages <= 1)
+    self.next_button.callback = self.next_page
+    self.add_item(self.next_button)
+
+  async def prev_page(self, interaction: discord.Interaction):
+    self.current_page = (self.current_page - 1) % self.total_pages
+    await self.update_page(interaction)
+
+  async def next_page(self, interaction: discord.Interaction):
+    self.current_page = (self.current_page + 1) % self.total_pages
+    await self.update_page(interaction)
+
+  async def update_page(self, interaction: discord.Interaction):
+    self.page_indicator.label = f"Page {self.current_page + 1:02}/{self.total_pages:02}"
+    await interaction.response.edit_message(
+      embed=self.embeds[self.current_page],
+      attachments=[self.pages[self.current_page]],
+      view=self
+    )
+
+async def generate_paginated_crystal_rarity_pages(user_id: int, rarity_level_crystals: list[dict]) -> tuple[list[discord.File], list[discord.Embed]]:
+  user = await bot.fetch_user(user_id)
+  page_size = 5
+  pages = []
+  embeds = []
+
+  if not rarity_level_crystals:
+    return pages, embeds
+
+  # We can simply get the name of the rarit and the emoji from the data embedded in the first crystal
+  rarity_name = rarity_level_crystals[0]['rarity_name']
+  rarity_emoji = rarity_level_crystals[0]['emoji']
+
+  for i in range(0, len(rarity_level_crystals), page_size):
+    chunk = rarity_level_crystals[i:i+page_size]
+    embed = discord.Embed(
+      title=f"{rarity_emoji} {rarity_name} Crystals",
+      description=f"Unattuned crystals in your Vault ()",
+      color=discord.Color.teal()
+    )
+    embed.set_footer(f"Page {(i // page_size + 1):02}")
+
+    for crystal in chunk:
+      embed.add_field(
+        name=f"{crystal['emoji']} {crystal['crystal_name']}",
+        value=crystal['description'],
+        inline=False
+      )
+
+    embeds.append(embed)
+    pages.append(discord.File(fp=b"", filename="placeholder.png"))  # Empty file placeholder for now
+
+  return pages, embeds
 
 #  ____ ___   __  .__.__
 # |    |   \_/  |_|__|  |   ______
