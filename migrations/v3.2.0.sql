22daacb26d4e9aa9ac7364d337e42250c2995fe3
zm <z@matting.ly>
2025-05-20 17:24:35 -0700

    lock and clear special badges from existing wishlists

--- migrations/v3.2.0.sql
+++ migrations/v3.2.0.sql
@@ -1,1 +1,5 @@
 UPDATE crystal_ranks SET name = 'unobtanium' WHERE LOWER(name) = 'unobtanium';
+
+UPDATE badge_instances SET locked = TRUE WHERE badge_info_id IN (SELECT id FROM badge_info WHERE is_special = TRUE);
+
+DELETE FROM badge_instances_wishlists WHERE badge_info_id IN ( SELECT id FROM badge_info WHERE is_special = TRUE);
