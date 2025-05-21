752e95d6aa532924dfafb71622b7cd1ebb4765a4
Zane Mattingly <z@matting.ly>
2025-05-20 18:43:50 -0700

    is_special is not exist, use just `special`

--- migrations/v3.2.0.sql
+++ migrations/v3.2.0.sql
@@ -1,5 +1,5 @@
 UPDATE crystal_ranks SET name = 'unobtanium' WHERE LOWER(name) = 'unobtanium';
 
-UPDATE badge_instances SET locked = TRUE WHERE badge_info_id IN (SELECT id FROM badge_info WHERE is_special = TRUE);
+UPDATE badge_instances SET locked = TRUE WHERE badge_info_id IN (SELECT id FROM badge_info WHERE special = TRUE);
 
-DELETE FROM badge_instances_wishlists WHERE badge_info_id IN ( SELECT id FROM badge_info WHERE is_special = TRUE);
+DELETE FROM badge_instances_wishlists WHERE badge_info_id IN ( SELECT id FROM badge_info WHERE special = TRUE);
