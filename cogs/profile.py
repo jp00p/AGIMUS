a800a2038172dd9c2f840b5aecac71a10ac6b202
Zane Mattingly <z@matting.ly>
2025-05-27 14:24:52 -0700

    show TOTAL badge count (all tiers) on /profile

--- cogs/profile.py
+++ cogs/profile.py
@@ -187,7 +187,7 @@ class Profile(commands.Cog):
     xp = echelon_progress.get('current_xp', 0)
     level = echelon_progress.get('current_level', 0)
     prestige_tier = echelon_progress.get('current_prestige_tier', 0)
-    badges = await db_get_user_badge_instances(member_id)
+    badges = await db_get_user_badge_instances(member_id, prestige=None)
     badge_count = len(badges)
     crystals = await db_get_user_attuned_and_harmonized_crystals(member_id)
     crystals_count = len(crystals)
