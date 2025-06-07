8f087c1bf05fdaea335e0a29f26ccd81c41e3c99
zm <z@matting.ly>
2025-06-07 13:54:17 -0700

    AGIMUS not AMIGUS

---
+++ migrations/v3.11.0.sql
@@ -0,0 +1,8 @@
+UPDATE badge_infoSET badge_name = 'AGIMUS Banner E', badge_filename = 'AGIMUS-Banner-E.png' WHERE badge_filename = 'AMIGUS-Banner-E.png';
+UPDATE badge_info SET badge_name = 'AGIMUS Banner D', badge_filename = 'AGIMUS-Banner-D.png' WHERE badge_filename = 'AMIGUS-Banner-D.png';
+UPDATE badge_affiliation SET badge_filename = 'AGIMUS-Banner-E.png' WHERE badge_filename = 'AMIGUS-Banner-E.png';
+UPDATE badge_affiliation SET badge_filename = 'AGIMUS-Banner-D.png' WHERE badge_filename = 'AMIGUS-Banner-D.png';
+UPDATE badge_type SET badge_filename = 'AGIMUS-Banner-E.png' WHERE badge_filename = 'AMIGUS-Banner-E.png';
+UPDATE badge_type SET badge_filename = 'AGIMUS-Banner-D.png' WHERE badge_filename = 'AMIGUS-Banner-D.png';
+UPDATE badge_universe SET badge_filename = 'AGIMUS-Banner-E.png' WHERE badge_filename = 'AMIGUS-Banner-E.png';
+UPDATE badge_universe SET badge_filename = 'AGIMUS-Banner-D.png' WHERE badge_filename = 'AMIGUS-Banner-D.png';
