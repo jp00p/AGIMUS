b1567c5f8c7f42e4f232bda2d6d7f374772f0912
zm <z@matting.ly>
2025-06-07 15:48:19 -0700

    rename AMIGUS files, add Lady Lurkana Coffee badge

--- migrations/v3.11.0.sql
+++ migrations/v3.11.0.sql
@@ -1,4 +1,4 @@
-UPDATE badge_infoSET badge_name = 'AGIMUS Banner E', badge_filename = 'AGIMUS-Banner-E.png' WHERE badge_filename = 'AMIGUS-Banner-E.png';
+UPDATE badge_info SET badge_name = 'AGIMUS Banner E', badge_filename = 'AGIMUS-Banner-E.png' WHERE badge_filename = 'AMIGUS-Banner-E.png';
 UPDATE badge_info SET badge_name = 'AGIMUS Banner D', badge_filename = 'AGIMUS-Banner-D.png' WHERE badge_filename = 'AMIGUS-Banner-D.png';
 UPDATE badge_affiliation SET badge_filename = 'AGIMUS-Banner-E.png' WHERE badge_filename = 'AMIGUS-Banner-E.png';
 UPDATE badge_affiliation SET badge_filename = 'AGIMUS-Banner-D.png' WHERE badge_filename = 'AMIGUS-Banner-D.png';
@@ -6,3 +6,5 @@ UPDATE badge_type SET badge_filename = 'AGIMUS-Banner-E.png' WHERE badge_filenam
 UPDATE badge_type SET badge_filename = 'AGIMUS-Banner-D.png' WHERE badge_filename = 'AMIGUS-Banner-D.png';
 UPDATE badge_universe SET badge_filename = 'AGIMUS-Banner-E.png' WHERE badge_filename = 'AMIGUS-Banner-E.png';
 UPDATE badge_universe SET badge_filename = 'AGIMUS-Banner-D.png' WHERE badge_filename = 'AMIGUS-Banner-D.png';
+UPDATE badge_info SET time_period = '2000s' WHERE badge_name = 'FOD Pride 2025';
+INSERT INTO badge_info (badge_name, badge_filename, badge_url, quadrant, time_period, franchise, reference, special) VALUES ("Lady Lurkana Coffee", "Lady_Lurkana_Coffee.png", "https://pennyante-art.carrd.co/", "Alpha", "2000s", "The USS Hood", "Lurqara' joH qa'vin, it's a Good Day to Rise. (Artwork by Penny Ante!)", 1);
