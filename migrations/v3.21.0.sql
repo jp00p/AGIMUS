9a148a9a8f60a3c72a2ee4875c36fdb63a1a17bd
Zane Mattingly <z@matting.ly>
2025-10-29 08:23:56 -0700

    FoDTober Badge (#686)
    * FoDTober special badge
    
    * Bump helm chart version to 3.20.3
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: jp00p <asimovsghost@gmail.com>

---
+++ migrations/v3.21.0.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO badge_info (badge_name, badge_filename, badge_url, quadrant, time_period, franchise, reference, special) VALUES ("FoD'Tober", "FoD'Tober.png", "https://pennyante-art.carrd.co/", "Alpha", "2000s", "The USS Hood", "Inky!", 1);
+INSERT IGNORE INTO badge_type (badge_filename, type_name) VALUES ("FoD'Tober.png", "Insignia");
+INSERT IGNORE INTO badge_universe (badge_filename, universe_name) VALUES ("FoD'Tober.png", "Prime");
+
