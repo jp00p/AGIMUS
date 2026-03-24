13fd09263aabbeb8df7afe73ece5a037cf4ffa52
Peter of the Norse <rahmcoff@gmail.org>
2026-03-23 18:02:29 -0600

    `get_member` does nothing

--- tasks/birthdays.py
+++ tasks/birthdays.py
@@ -36,7 +36,7 @@ def birthdays_task(bot: Bot):
     user_ids = await db.get_users_with_birthday(today.month, today.day)
     description = [
       # This assumes that the first guild is the only one, but that is a common assumption all around AGIMUS
-      (f"<@{uid}>" if bot.guilds[0].get_member(uid) else await db.clear_birthday(uid)) for uid in user_ids
+      (f"<@{uid}>" if await bot.guilds[0].fetch_member(uid) else await db.clear_birthday(uid)) for uid in user_ids
     ]
 
     celebrities = trek_birthdays[today.strftime('%b')].get(today.strftime('%d'), [])
@@ -48,7 +48,7 @@ def birthdays_task(bot: Bot):
     if today.month == 2 and today.day == 28 and today.year % 4 > 0:
       user_ids = await db.get_users_with_birthday(2, 29)
       description.extend(
-        (f"<@{uid}>" if bot.guilds[0].get_member(uid) else await db.clear_birthday(uid)) for uid in user_ids
+        (f"<@{uid}>" if await bot.guilds[0].fetch_member(uid) else await db.clear_birthday(uid)) for uid in user_ids
       )
 
     if len(description) == 0:
